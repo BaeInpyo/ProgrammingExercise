ae4fbd569b6c58ad604cb57b43af1825641ee90f
BaeInpyo <dkas2@snu.ac.kr>
2019-06-12 23:24:23 +0900

    Adjust tab and space

--- Chapter6/vip_picnic.cpp
+++ Chapter6/vip_picnic.cpp
@@ -8,54 +8,54 @@ bool assign[N] = { false };
 bool areFriends[N][N] = { { false } };
 
 int findPair(int n) {
-	int candidate, mate;
-	int cases;
+    int candidate, mate;
+    int cases;
 
-	if (n < 2) {
-		for (candidate = 0; candidate < N; candidate++) {
-			if (!assign[candidate]) return 0;
-		}
-		return 1;
-	}
+    if (n < 2) {
+        for (candidate = 0; candidate < N; candidate++) {
+            if (!assign[candidate]) return 0;
+        }
+        return 1;
+    }
 
-	for (candidate = 0; candidate < N; candidate++) {
-		if (!assign[candidate]) {
-			assign[candidate] = true;
-			break;
-		}
-	}
+    for (candidate = 0; candidate < N; candidate++) {
+        if (!assign[candidate]) {
+            assign[candidate] = true;
+            break;
+        }
+    }
 
-	for (mate = candidate+1; mate < N; mate++) {
-		if (areFriends[candidate][mate] && !assign[mate]) {
-			assign[mate] = true;
-			cases += findPair(n-2);
-			assign[mate] = false;
-		}
-	}
+    for (mate = candidate+1; mate < N; mate++) {
+        if (areFriends[candidate][mate] && !assign[mate]) {
+            assign[mate] = true;
+            cases += findPair(n-2);
+            assign[mate] = false;
+        }
+    }
 
-	assign[candidate] = false;
+    assign[candidate] = false;
 
-	return cases;
+    return cases;
 }
 
 int main() {
-	int n, m;
-	int x, y;
+    int n, m;
+    int x, y;
 
-	cin >> n; cin >> m;
+    cin >> n; cin >> m;
 
-	for (int i = n; i < N; i++) {
-		assign[i] = true;
-	}
+    for (int i = n; i < N; i++) {
+        assign[i] = true;
+    }
 
-	for (int i = 0; i < m; i++) {
-		cin >> x; cin >> y;
-		if (x < y) {
-			areFriends[x][y] = true;
-		} else {
-			areFriends[y][x] = true;
-		}
-	}
+    for (int i = 0; i < m; i++) {
+        cin >> x; cin >> y;
+        if (x < y) {
+            areFriends[x][y] = true;
+        } else {
+            areFriends[y][x] = true;
+        }
+    }
 
-	cout << "Result: " << findPair(n) << endl;
+    cout << "Result: " << findPair(n) << endl;
 }
