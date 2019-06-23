a4c3b75435ba859b67bb31716d60b00e88dff597
BaeInpyo <dkas2@snu.ac.kr>
2019-06-23 11:21:46 +0900

    solve fence problem

--- Chapter7_DivideConquer/Fence/vip_fence.cpp
+++ Chapter7_DivideConquer/Fence/vip_fence.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int total_cnt;
+vector<int> fence;
+
+int find_rectangle() {
+    int area = -1;
+    for (int i = 0; i < total_cnt; i++) {
+        int height = fence[i];
+        int width = 1;
+
+        // left
+        for (int left = i-1; left >= 0 && fence[left] >= height; left--) {
+            width++;
+        }
+
+        // right
+        for (int right = i+1; right < total_cnt && fence[right] >= height; right++) {
+            width++;
+        }
+
+        area = max(area, height*width);
+    }
+    return area;
+}
 
 int main() {
+    cin >> total_cnt;
+
+    for (int i = 0; i < total_cnt; i++) {
+        int height;
+        cin >> height;
+        fence.push_back(height);
+    }
+
+    cout << find_rectangle() << endl;
     return 0;
 }
