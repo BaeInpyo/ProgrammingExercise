4d23251e0f0b214916776765e594211e216e9d8b
BaeInpyo <dkas2@snu.ac.kr>
2019-06-29 22:46:27 +0900

    Solve Wildcard problem
    Ugly code ... :-1:

--- Chapter8_DP/Wildcard/vip_wildcard.cpp
+++ Chapter8_DP/Wildcard/vip_wildcard.cpp
@@ -1,86 +1,88 @@
 #include <iostream>
-#include <vector>
 
-#define WILDCARD_ANY '*'
-#define WILDCARD_ONE '?'
+#define MAX_FILE 50
+#define MAX_STR 100
 
 using namespace std;
 
 int num_files;
 string pattern;
-vector<string> files;
+int pattern_ast[MAX_STR];
+int num_ast = 0;
+string files[MAX_FILE];
 
-pair<int,int> find_substring(string file, int file_idx, string pattern, int pattern_idx, bool from_wildcard) {
-    int file_start=file_idx;
-    int pattern_start=pattern_idx;
+pair<bool, int> compare_string(int pattern_start, int pattern_end, int file_start, int file_idx, bool is_ast) {
+    string file = files[file_idx];
+    char pattern_first = pattern[pattern_start];
+    bool found_next = false;
+    int next = file_start + 1;
 
-    if (!from_whildcard) {
-        while(pattern_idx < pattern.length() && pattern[pattern_idx] != '*') {
-            if (pattern[pattern_idx] != '?' && pattern[pattern_idx] != file[file_idx]) {
-                return make_pair(0,0);
-            }
-            
-            pattern_idx++; file_idx++;
+    for (int i = pattern_start; i < pattern_end; i++) {
+        if (file_start >= file.length()) { return make_pair(false, file_start); }
+
+        char file_i = file[file_start];
+        char pattern_i = pattern[i];
+        if (!(pattern_i == '?' || pattern_i == file_i)) { return make_pair(false, next); }
+
+        // 두번째 비교부터 만약 패턴 처음 문자랑 같은 순간이 오면 그 위치 최초만 기록 (caching)
+        if (i != pattern_start && file_i == pattern_first && !found_next) {
+            found_next = true;
+            next = file_start;
         }
-    } else {
-        while(file_idx < file.length() && pattern_idx < pattern.length() && pattern[pattern_idx] != '*') {
-            if (pattern[pattern_idx] == file[file_idx]) {
-                if (file_idx + 1 < file.length() && pattern[pattern_idx] != file[file_idx+1]) {
-                    pattern_idx++;
-                }
-            } else if (pattern[pattern_idx] == '?') {
-                pattern_idx++;
-            }
-            file_idx++;
-        }
+
+        file_start++;
     }
     
-    return make_pair(file_idx, pattern_idx);
+    if (!is_ast && file_start != file.length()) return make_pair(false, next);
+    return make_pair(true, file_start);
+}
+
+void find_asterisk() {
+    int idx = 0;
+    for (int i = 0; i < MAX_STR; i++) {
+        pattern_ast[i] = -1;
+    }
+
+    for (int i = 0; i < pattern.length(); i++) {
+        if (pattern[i] == '*') {
+            pattern_ast[idx++] = i;
+            num_ast++;
+        }
+    }
 }
 
 void find_pattern() {
-    for (auto it = files.begin(); it != files.end(); ++it) {
-        string file = *it;
+    for (int i = 0; i < num_files; i++) {
+        int pattern_start = 0, file_start = 0, pattern_end = pattern.length();
+        bool strict_compare = true;
+        pair<bool, int> result;
 
-        int pattern_idx = 0;
-        int file_idx = 0;
-        pair<int,int> next_pos;
-        bool from_wildcard = false;
-
-        while (file_idx < file.length()) {
-            if (pattern_idx == pattern.length()) {
-                break;
-            }
-
-            char pattern_char = pattern[pattern_idx];
-            //cout << file[file_idx] << " & " << pattern[pattern_idx] << endl;
-            if (pattern_char == '*') {
-                pattern_idx++;
-                from_wildcard = true;
-                continue;
-            }
-
-            next_pos = find_substring(file, file_idx, pattern, pattern_idx, from_wildcard);
-            int file_next_pos = next_pos.first();
-            int pattern_next_pos = next_pos.second();
-
-            if (!file_next_pos) {
-                file_idx += 1;
+        for (int j = 0; j < num_ast; j++) {
+            pattern_end = pattern_ast[j];
+            if (pattern_end == 0) {
+                pattern_start = 1;
+                strict_compare = false;
             } else {
-                file_idx = file_next_pos;
-                pattern_idx = pattern_next_pos;
+                while (true) {
+                    result = compare_string(pattern_start, pattern_end, file_start, i, true);
+                    file_start = result.second;
+                    if (result.first) {
+                        strict_compare = false;
+                        pattern_start = pattern_end + 1;
+                        break;
+                    } else {
+                        if (strict_compare) return;
+                        if (files[i].length() - file_start < pattern_end - pattern_start) return;
+                    }
+                }
             }
         }
 
-        if (pattern_idx == pattern.length()) {
-            if (!(pattern[pattern_idx-1] != '*' && file_idx != file.length())) {
-                cout << file << endl;
-            }
-        } else {
-            if (pattern[pattern_idx] == '*') {
-                cout << file << endl;
-            }
+        if (pattern_end != pattern.length()-1) {
+            result = compare_string(pattern_start, pattern.length(), file_start, i, false);
         }
+
+        if (result.first) { cout << files[i] << endl; }
     }
 }
 
@@ -88,13 +90,13 @@ int main() {
     getline(cin, pattern);
 
     cin >> num_files;
-
-    for (int i = 0; i <= num_files; i++) {
+    cin.ignore();
+    for (int i = 0; i < num_files; i++) {
         string file;
         getline(cin, file);
-        if (i != 0) files.push_back(file);
+        files[i] = file;
     }
-
+    find_asterisk();
     find_pattern();
 
     return 0;
