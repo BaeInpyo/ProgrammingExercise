330615a3d1e20529533d80eff598ecaaebfd59e2
BaeInpyo <dkas2@snu.ac.kr>
2019-07-01 13:32:18 +0900

    Solve Pi problem but need to implement memoization

--- Chapter8_DP/Pi/vip_pi.cpp
+++ Chapter8_DP/Pi/vip_pi.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#define MAX 50000
+
 using namespace std;
 
 int num_cases;
@@ -14,7 +16,7 @@ int get_difficulty(int start, int end, string number) {
     int diff = a - b;
     if (diff == 0) res = 1;                     // first
     else if (diff == 1 || diff == -1) res = 2;  // second
-    else res = 4;                               // fourth
+    else res = 5;                               // fourth
     for (int i = start+2; i < end; i++) {
         int c = number[i] - '0';
         if (b - c == diff) { b = c; } 
@@ -28,18 +30,29 @@ int get_difficulty(int start, int end, string number) {
         int c = number[i] - '0';
         if ((i-start) % 2 == 0) {
             if(a != c) { res = 0; break; }
-            else { res = 3; }
+            else { res = 4; }
         } else {
             if(b != c) { res = 0; break; }
-            else { res = 3; }
+            else { res = 4; }
         }
     }
     if (res) return res;
-    else return 5;                              // last
+    else return 10;                              // last
 }
 
-int solution(int start, int index) {
-    get_difficulty(start, 5, numbers[index]);
+int solution(int start, string number) {
+    int three = MAX, four = MAX, five = MAX;
+    int len = number.length() - start;
+    if (len <= 5) {
+        return get_difficulty(start, number.length(), number);
+    }
+
+    three = get_difficulty(start, start+3, number) + solution(start+3, number);
+
+    if (len > 6) four  = get_difficulty(start, start+4, number) + solution(start+4, number);
+    if (len > 7) five  = get_difficulty(start, start+5, number) + solution(start+5, number);
+
+    return min(min(three, four), five);
 }
 
 int main () {
@@ -50,7 +63,7 @@ int main () {
     }
 
     for (int i = 0; i < num_cases; i++) {
-        cout << solution(0, i) << endl;
+        cout << solution(0, numbers[i]) << endl;
     }
 
     return 0;
