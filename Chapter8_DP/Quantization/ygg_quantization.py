8dc3e953ef8d15c6f985e4a40bbf8f7e639c5651
ene5135 <ene5135@naver.com>
2019-07-25 01:21:56 +0900

    Update ygg_quantization.py
    부분합 추가, 런타임에러 여전

--- Chapter8_DP/Quantization/ygg_quantization.py
+++ Chapter8_DP/Quantization/ygg_quantization.py
@@ -1,18 +1,32 @@
 #!/bin/python3
 
 import sys
-import random
+#import random
 import math
-from functools import reduce
 sys.setrecursionlimit(1500)
 envDict = {} # key: (start,s), value: err
+pSum = {}
+pSqSum = {}
 
-# 숫자 리스트 => 평균값, 평균값에 대한 에러
-def getErrWithMean(numbers):
-    mean = round(sum(numbers)/len(numbers))
-    currErr = reduce(lambda x,y: x+(mean-y)**2, numbers, 0)
-    return mean, currErr
+# 부분합 초기화
+def initSum(numbers):
+    pSum[0], pSqSum[0] = numbers[0], numbers[0]**2
+    for i in range(1,len(numbers)):
+        pSum[i] = pSum[i-1]+numbers[i]
+        pSqSum[i] = pSqSum[i-1]+numbers[i]**2
 
+# 구간의 평균에대한 에러를 반환
+def getErrWithMean(fr, to, numbers):
+    if fr == 0:
+        partialSum = pSum[to]
+        partialSqSum = pSqSum[to]
+    else:
+        partialSum = pSum[to] - pSum[fr-1]
+        partialSqSum = pSqSum[to] - pSqSum[fr-1]
+
+    mean = round(partialSum/(to-fr+1))
+    currErr = partialSqSum - 2*mean*partialSum + (mean**2)*(to-fr+1)
+    return currErr
 
 def solution(start, s, numbers):
     minErr = math.inf
@@ -28,15 +42,13 @@ def solution(start, s, numbers):
 
     # s가 1이면 재귀콜 필요없이 남은 모든 숫자에서 에러구함
     if s == 1:
-        currNumbers = numbers[start:]
-        _, currErr = getErrWithMean(currNumbers)
+        currErr = getErrWithMean(start, len(numbers)-1, numbers)
         envDict[(start,s)] = currErr
         return currErr
 
     # 첫번째 묶음의 크기를 키워가며 에러를 비교함
     for i in range(start, len(numbers)-s+1):
-        currNumbers = numbers[start:i+1]
-        mean, currErr = getErrWithMean(currNumbers)
+        currErr = getErrWithMean(start, i, numbers)
         minErr = min(minErr, currErr+solution(i+1, s-1, numbers))
 
     # 최종적으로 해당 환경에대한 최소오차를 기억하고 반환
@@ -46,7 +58,7 @@ def solution(start, s, numbers):
 if __name__ == '__main__':
     C = int(sys.stdin.readline())
     for _ in range(C):
-        envDict = {}
+        envDict, pSum, pSqSum = {}, {}, {}
         n, s = list(map(int, sys.stdin.readline().rstrip().split()))
         numbers = sys.stdin.readline().rstrip()
         if len(numbers) > 1:
@@ -54,5 +66,6 @@ if __name__ == '__main__':
         else:
             numbers = [int(numbers)]
         numbers.sort()
+        initSum(numbers)
         print(solution(0, s, numbers))
         #print(' '.join([str(random.randrange(1,1000)) for i in range(100)]))
