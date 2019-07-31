406cf36912d90104bc9f2aa3561e5e83e2856cf2
bmy4415 <bmy4415@naver.com>
2019-07-31 23:25:41 +0900

    numb3rs 오답 코드

---
+++ Chapter8_DP/Numb3rs/sjw_numb3rs.py
@@ -0,0 +1,48 @@
+import sys
+
+
+def solution(n, d, p, A, Q):
+    probability = [0] * n
+    probability[p] = 1
+    degree = [sum(row) for row in A]
+    adj_list = []
+
+    # initialize adjacent list
+    for row in A:
+        adj_list.append([x[0] for x in enumerate(row) if x[1] == 1])
+
+    for _ in range(d):
+
+        new_probability = [0] * n
+
+        for curr in range(n):
+            adj_nodes = adj_list[curr]
+
+            for node in adj_nodes:
+                new_probability[node] += probability[curr] / degree[curr] # propagate probability
+
+        probability = new_probability
+
+    return [round(probability[x], 8) for x in Q]
+
+
+if __name__ == "__main__":
+    # freopen equivalent
+    sys.stdin = open("input.txt", "r")
+
+    c = int(sys.stdin.readline().strip())
+    answers = []
+    for _ in range(c):
+        n, d, p = [int(x) for x in sys.stdin.readline().strip().split()]
+        A = []
+
+        for __ in range(n):
+            A.append([int(x) for x in sys.stdin.readline().strip().split()])
+
+        t = int(sys.stdin.readline().strip())
+        Q = [int(x) for x in sys.stdin.readline().strip().split()]
+
+        answers.append(solution(n, d, p, A, Q))
+
+    for a in answers:
+        print(a)
