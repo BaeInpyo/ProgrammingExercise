4c6d4d8fa29421761d5e86c24664370f8c0856a1
BaeInpyo <dkas2@snu.ac.kr>
2019-08-10 20:12:05 +0900

    First version of ocr

--- Chapter9_DPTechnique/Ocr/vip_ocr.cpp
+++ Chapter9_DPTechnique/Ocr/vip_ocr.cpp
@@ -1,4 +1,4 @@
-#include <iostream.h>
+#include <iostream>
 
 #define M 500
 #define Q 20
@@ -15,10 +15,70 @@ double probs_of_next_appear[M][M];
 double probs_of_decoding[M][M]; 
 
 int num_words_in_str;
-string test_str[M];
+string test_str[N];
+int str_idx[N];
+
+double prob_cache[N][M];
+int path_cache[N][M];
+void init_cache() {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            prob_cache[i][j] = -1;
+            path_cache[i][j] = -1;
+        }
+    }
+}
+/*
+ * ocr(n, i) = max_{0<=j<m}(T[j][i] * M[words(n)][i] * ocr(n-1, j))
+ *   0 <= n < N
+ *   0 <= i < M
+ */
+double ocr(int n, int i) {
+    double &res_prob = prob_cache[n][i];
+    int &res_path = path_cache[n][i];
+    if (n == 0) {
+        res_prob = probs_of_first_appear[i] * probs_of_decoding[str_idx[n]][i];
+        return res_prob;
+    }
+
+    if (res_prob != -1) return res_prob;
+
+    for (int j = 0; j < num_words; ++j) {
+        double temp_prob = probs_of_next_appear[j][i] * probs_of_decoding[str_idx[n]][i] * ocr(n-1, j);
+        if (res_prob < temp_prob) {
+            res_prob = temp_prob;
+            res_path = j;
+        }
+    }
+    return res_prob;
+}
+
+void print_answer(int path) {
+    string result = words[path];
+
+    for (int i = num_words_in_str-1; i > 0; ++i) {
+        path = path_cache[i][path];
+        result = path + " " + result;
+    }
+
+    cout << result << endl;
+
+    return;
+}
 
 void solution() {
-    
+    double res_prob = -1;
+    int res_path;
+    init_cache();
+    for (int i = 0; i < num_words; ++i) {
+        double temp_prob = ocr(num_words_in_str-1, i);
+        if (res_prob < temp_prob) {
+            res_prob = temp_prob;
+            res_path = i;
+        }
+    }
+
+    print_answer(res_path);
 }
 
 int main () {
@@ -48,6 +108,11 @@ int main () {
         cin >> num_words_in_str;
         for (int i = 0; i < num_words_in_str; ++i) {
             cin >> test_str[i];
+            for (int j = 0; j < num_words; ++j) {
+                if (test_str[i].compare(words[j])) {
+                    str_idx[i] = j; break;
+                }
+            }
         }
 
         solution();
