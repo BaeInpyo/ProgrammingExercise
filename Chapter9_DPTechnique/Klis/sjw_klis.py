cdf4e7f73b230a72baf6d8d1bd3ae7b0bc59fb0a
bmy4415 <bmy4415@naver.com>
2019-08-18 17:02:57 +0900

    klis 시간초과

--- Chapter9_DPTechnique/Klis/sjw_klis.py
+++ Chapter9_DPTechnique/Klis/sjw_klis.py
@@ -2,23 +2,87 @@ import sys
 
 
 """
-전략
+At first glance, we can calculate LIS by finding all sub-sequences and conditioning IS(Increasing Sub-sequence) and
+calculate maximum length. But there can be 2^n sub-sequences, it's intractable.
 
-주어진 list A에 대해서 LIS는 다음의 성질을 만족한다.
-1) cache[i] = Ai로 끝나는 LIS의 길이라고 하자.
-2) cache[i] = Ak < Ai, k < i를 만족하는 k중 Ak가 가장큰 k들에 대해 cache[k] + 1
+LIS has next characteristic.
+A: given number list
+cache[i] = length of LIS that ends up with Ai
+cache[i] = max(cache[k]) + 1 where k < i and Ak < Ai
 
-2)의 조건을 만족하는 k가 여러개인 경우 LIS를 여러개 만들 수 있다.
-cache_recon을 이용하여 cache_recon[i] = cache[i]를 만들게한 k값들의 list 로 저장을 하면
-cache와 cache_recon을 만든 후 cache_recon[n]부터 시작하여 모든 LIS를 구할 수 있다.
+We can use cache_recon to re-construct LIS and cache_recon has following characteristic.
+cache_recon[i] = list of ks that made cache[i] 
 
-이 LIS를 사전순으로 정렬하면 KLIS를 구할 수 있다.
-정렬을 하지 않아도, 각 분기마다 몇개씩 갈래가 있는지 알 수 있으므로 KLIS를 단번에 구할 수 있다.
+Since we want to find Kth LIS itself, we should use cache_recon.
+Instead of finding all LISs and sort it, calculate number of LISs start with given number.
 """
 
 
+def start_with_num(num, length, cache_recon, num_to_idx, idx_to_num):
+    if length == 1:
+        return 1
+
+    result = 0
+    start_index = num_to_idx[num]
+    for i in range(start_index+1, len(cache_recon)):
+        curr = cache_recon[i]
+        if start_index in curr:
+            result += start_with_num(idx_to_num[i], length-1, cache_recon, num_to_idx, idx_to_num)
+
+    return result
+
+
+def find_all_LIS(num, length, cache_recon, num_to_idx, idx_to_num):
+    if length == 1:
+        return [[num]]
+
+    candidates = []
+    start_index = num_to_idx[num]
+    for i in range(start_index+1, len(cache_recon)):
+        curr = cache_recon[i]
+        if start_index in curr:
+            candidates.extend(find_all_LIS(idx_to_num[i], length-1, cache_recon, num_to_idx, idx_to_num))
+
+    return [[num] + x for x in candidates]
+
+
 def solution(N, K, arr):
-    pass
+    num_to_idx = { x[1]: x[0] for x in enumerate(arr) }
+    idx_to_num = arr
+
+    # initialize cache
+    cache = [1] * N
+    cache_recon = [[i] for i in range(N)]
+
+    # fill cache
+    for i in range(1, N):
+        candidates = []
+        for j in range(i):
+            if arr[j] < arr[i]:
+                candidates.append((j, cache[j]))
+
+        if not candidates:
+            continue
+
+        candidates.sort(key=lambda x:x[0])
+        max_length = max([x[1] for x in candidates]) + 1
+        max_indexes = [x[0] for x in candidates if x[1] == max_length-1]
+
+        cache[i] = max_length
+        cache_recon[i] = max_indexes
+
+    LIS_length = max(cache)
+    k = 0
+    # find kth LIS
+    for num in sorted(arr):
+        swn = start_with_num(num, LIS_length, cache_recon, num_to_idx, idx_to_num)
+        if k < K <= k + swn:
+            all_LISs = find_all_LIS(num, LIS_length, cache_recon, num_to_idx, idx_to_num)
+            all_LISs.sort()
+            return all_LISs[K-k-1]
+
+        else:
+            k += swn
 
 
 if __name__ == "__main__":
