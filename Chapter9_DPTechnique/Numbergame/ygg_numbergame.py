a1bcdc86f8313a9ae9db73c7aa8d8974d2dd74e2
ene5135 <ene5135@naver.com>
2019-09-22 18:42:16 +0900

    정답, 다음부터는 재귀콜을 식으로 적어보고 짜자.

--- Chapter9_DPTechnique/Numbergame/ygg_numbergame.py
+++ Chapter9_DPTechnique/Numbergame/ygg_numbergame.py
@@ -1,46 +1,39 @@
+import sys
+
 env_dict = {}
 
-def solution(numbers, turn, H_score, S_score):
+def solution(numbers):
     length = len(numbers)
-    case1 = -99999999 if turn else 99999999
-    case2 = -99999999 if turn else 99999999
+    case1 = -99999999
+    case2 = -99999999
     if length == 0:
-        return H_score - S_score
+        return 0
 
-    if env_dict.get((turn, tuple(numbers)), -1) != -1:
-        return env_dict[(turn, tuple(numbers))]
+    if env_dict.get((tuple(numbers)), -1) != -1:
+        return env_dict[(tuple(numbers))]
 
-    if turn:
-        if length >= 2:
-            # 왼쪽 두개 지우기
-            case1 = solution(numbers[2:], (turn+1)%2, H_score, S_score)
-            # 오른쪽 두개 지우기
-            case2 = solution(numbers[:-2], (turn+1)%2, H_score, S_score)
-        # 왼쪽 하나 선택 or 오른쪽 하나 선택
-        case3 = solution(numbers[1:], (turn+1)%2, H_score+numbers[0], S_score)
-        case4 = solution(numbers[:-1], (turn+1)%2, H_score+numbers[-1], S_score)
-        result = max(case1,case2,case3,case4)
+    if length >= 2:
+        # 왼쪽 두개 지우기
+        case1 = -solution(numbers[2:])
+        # 오른쪽 두개 지우기
+        case2 = -solution(numbers[:-2])
+    # 왼쪽 하나 선택 or 오른쪽 하나 선택
+    case3 = numbers[0] - solution(numbers[1:])
+    case4 = numbers[-1] - solution(numbers[:-1])
+    result = max(case1,case2,case3,case4)
 
-    else:
-        if length >= 2:
-            # 왼쪽 두개 지우기
-            case1 = solution(numbers[2:], (turn+1)%2, H_score, S_score)
-            # 오른쪽 두개 지우기
-            case2 = solution(numbers[:-2], (turn+1)%2, H_score, S_score)
-        # 왼쪽 하나 선택 or 오른쪽 하나 선택
-        case3 = solution(numbers[1:], (turn+1)%2, H_score, S_score+numbers[0])
-        case4 = solution(numbers[:-1], (turn+1)%2, H_score, S_score+numbers[-1])
-        result = min(case1,case2,case3,case4)
+    env_dict[(tuple(numbers))] = result
 
     return result
             
     
     
 if __name__ == '__main__':
-    C = int(input().rstrip())
+    #sys.stdin = open('input.in', 'r')
+    C = int(sys.stdin.readline().rstrip())
     for _ in range(C):
         env_dict = {}
-        n = input().rstrip()
-        numbers = list(map(int, input().rstrip().split()))
-        result = solution(numbers, 1, 0, 0)
+        n = sys.stdin.readline().rstrip()
+        numbers = list(map(int, sys.stdin.readline().rstrip().split()))
+        result = solution(numbers)
         print(result)
