061cb4ab1c317e3ee523662c34e96e37c21c392b
BaeInpyo <dkas2@snu.ac.kr>
2019-09-23 01:05:40 +0900

    Finish blockgame

--- Chapter9_DPTechnique/Blockgame/vip_blockgame.cpp
+++ Chapter9_DPTechnique/Blockgame/vip_blockgame.cpp
@@ -7,103 +7,87 @@
 #define MAX   5
 #define EMPTY '.'
 #define FILL  '#'
+#define WIN   2
+#define LOSE  1
+#define NAN   0
 
 using namespace std;
 
 int num_tests;
-char board[MAX][MAX];
-int encrypted_board = 0;
 int blocks[6][3][2] = { {{0, 0}, {0, 1}, {-1, -1}},  // len: 2
                         {{0, 0}, {1, 0}, {-1, -1}},  // len: 2
                         {{0, 0}, {1, 0}, {1, 1}},    // len: 3
                         {{0, 0}, {0, 1}, {1, 1}},    // len: 3
                         {{0, 0}, {0, 1}, {1, 0}},    // len: 3
                         {{0, 0}, {1, 0}, {1, -1}} }; // len: 3
-int cache_test_num[BOARD_IN_INT];
-bool cache[BOARD_IN_INT];
+char cache[BOARD_IN_INT];
 
 void init_cache() {
   for (int i = 0; i < BOARD_IN_INT; ++i) {
-    cache_test_num[i] = -1;
+    cache[i] = 0;
   }
 }
 
 int encrypt_cell(int x, int y) {
-  int cell = 1 << y;
-  cell = cell << x * MAX;
-  return cell;
+  return 1 << x * MAX + y;
 }
 
-bool is_empty_cell(int x, int y) {
+bool is_empty_cell(int board, int x, int y) {
   int encrypted_cell = encrypt_cell(x, y);
 
-  return ((encrypted_board & encrypted_cell) == 0);
+  return ((board & encrypted_cell) == 0);
 }
 
-void encrypt_board() {
-  for (int i = 0; i < MAX; ++i) {
-    for (int j = 0; j < MAX; ++j) {
-      if (board[i][j] == FILL) {
-        encrypted_board += encrypt_cell(i, j);
-      }
-    }
-  }
-}
-
-bool can_insert_block(int x, int y, int block_index) {
-  bool result = true;
+bool can_insert_block(int board, int x, int y, int block_index) {
   for (int parts = 0; parts < 3; ++parts) {
     if (block_index < 2 && parts == 2) break;
     int loc_x = x+blocks[block_index][parts][0];
     int loc_y = y+blocks[block_index][parts][1];
-    if (0 <= loc_x && loc_x < MAX && 0 <= loc_y && loc_y < MAX) {
-      result = result && is_empty_cell(loc_x, loc_y);
-    } else {
-      result = false;
-    }
+    bool range = (0 <= loc_x && loc_x < MAX && 0 <= loc_y && loc_y < MAX);
+    if (!range) return false;
+    if (!is_empty_cell(board, loc_x, loc_y)) return false;
   }
   
-  return result;
+  return true;
 }
 
-void modify_block(int x, int y, int block_index, bool insert) {
+int insert_cell(int board, int x, int y, int block_index) {
   for (int parts = 0; parts < 3; ++parts) {
     if (block_index < 2 && parts == 2) break; 
     int loc_x = x+blocks[block_index][parts][0];
     int loc_y = y+blocks[block_index][parts][1];
     
-    if (insert) encrypted_board += encrypt_cell(loc_x, loc_y);
-    else encrypted_board -= encrypt_cell(loc_x, loc_y);
+    board |= encrypt_cell(loc_x, loc_y);
   }
+  return board;
 }
 
-bool can_win() {
-  int &test_num = cache_test_num[encrypted_board];
-  bool &res = cache[encrypted_board];
-
-  if (test_num == num_tests) return res;
+char can_win(int board) {
+  char &res = cache[board];
+  if (res != NAN) return res;
 
   // Main algorithm
-  test_num = num_tests;
-  res = false;
+  res = LOSE;
   for (int i = 0; i < MAX; ++i) {
     for (int j = 0; j < MAX; ++j) {
+      if (!is_empty_cell(board, i, j)) continue;
       for (int block_index = 0; block_index < 6; ++block_index) {
-        if (can_insert_block(i, j, block_index)) {
-          modify_block(i, j, block_index, true);
-          if (!can_win()) res = true;
-          modify_block(i, j, block_index, false);
+        if (can_insert_block(board, i, j, block_index)) {
+          int next_board = insert_cell(board, i, j, block_index);
+          if (can_win(next_board) == LOSE) {
+            res = WIN;
+            return res;
+          }
         }
       }
     }
   }
-
+  
   return res;
 }
 
-void solution() {
-  encrypt_board();
-  if (can_win()) { cout << "WINNING" << endl; }
+void solution(int board) {
+  if (can_win(board) == WIN) { cout << "WINNING" << endl; }
   else { cout << "LOSING" << endl; }
 }
 
@@ -112,16 +96,16 @@ int main() {
   
   cin >> num_tests;
 
-  init_cache();
-
   while (--num_tests >= 0) {
+    int board = 0;
     for (int r = 0; r < MAX; ++r) {
       for (int c = 0; c < MAX; ++c) {
-        cin >> board[r][c];
+        char cell; cin >> cell;
+        if (cell == FILL) board += encrypt_cell(r, c);
       }
     }
     
-    solution();
+    solution(board);
   }
 
   return 0;
