3ae38c1b5fd1c12267c20a344c9deb93a59a5b75
BaeInpyo <dkas2@snu.ac.kr>
2019-10-01 19:02:17 +0900

    Main algorithm (recursion) left

--- Chapter9_DPTechnique/Restore/vip_restore.cpp
+++ Chapter9_DPTechnique/Restore/vip_restore.cpp
@@ -5,26 +5,30 @@ using namespace std;
 
 int num_tests;
 int k;
-string words[15];
-bool distinct_words[15];
+int num_distinct_words;
+string input_words[15];
+string distinct_words[15];
+bool subset[15];
+int cache[15][1 << 14];
 
-void init_array() {
+void init() {
+  num_distinct_words = 0;
   for (int i = 0; i < 15; ++i) {
-    distinct_words[i] = true;
+    subset[i] = false;
   }
 }
 
 // Check whether i word is subset of j word.
 bool check_subset(int i, int j) {
   bool result = false;
-  string i_words = words[i];
-  string j_words = words[j];
+  string i_words = input_words[i];
+  string j_words = input_words[j];
 
   if (i_words.length() <= j_words.length()) {
     size_t found = j_words.find(i_words);
     if (found != string::npos) {
       result = true;
-      distinct_words[i] = false;
+      subset[i] = true;
     }
   }
 
@@ -38,23 +42,62 @@ void preprocess() {
       if (check_subset(i, j)) break;
     }
   }
+
+  for (int i = 0; i < k; ++i) {
+    if (!subset[i]) distinct_words[num_distinct_words++] = input_words[i];
+  }
+}
+
+int calc_remains(int index, int total) {
+  return total - (1 << index);
+}
+
+bool exists(int index, int remains) {
+  return ((remains & (1 << index)) > 0);
+}
+
+int find_shortest(int start, int remains) {
+  if (remains == 0) {
+    return distinct_words[start].size();
+  }
+
+  int &res = cache[start][remains];
+
+  if (res != -1) return res;
+
+  return res;
 }
 
 void solution() {
-  init_array();
+  init();
   // Check whether a word is subset of other words and remove it
   preprocess();
+
+  int index = -1;
+  int min_len = 100000;
+  int total = (1<<num_distinct_words) - 1;
+  for (int i = 0; i < num_distinct_words; ++i) {
+    int remains = calc_remains(i, total);
+    int len = find_shortest(i, remains);
+    if (len < min_len) {
+      index = i;
+      min_len = len;
+    }
+  }
+
+  cout << index << endl;
+  cout << min_len << endl;
 }
 
 int main() {
-    cin.sync_with_stdio(false);
-    cin >> num_tests;
-    while (--num_tests >= 0) {
-        cin >> k;
-        for (int i = 0; i < k; i++) {
-            cin >> words[i];
-        }
-
-        solution();
+  cin.sync_with_stdio(false);
+  cin >> num_tests;
+  while (--num_tests >= 0) {
+    cin >> k;
+    for (int i = 0; i < k; i++) {
+      cin >> input_words[i];
     }
+
+    solution();
+  }
 }
