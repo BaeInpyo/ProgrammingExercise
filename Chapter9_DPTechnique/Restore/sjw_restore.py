14b8d6f11e00a8457e77b000b4e60351c84b9834
bmy4415 <bmy4415@naver.com>
2019-10-01 13:03:30 +0900

    sjw - solve RESTORE

--- Chapter9_DPTechnique/Restore/sjw_restore.py
+++ Chapter9_DPTechnique/Restore/sjw_restore.py
@@ -1,69 +1,90 @@
 import sys
-import string
-from itertools import combinations, chain
-
+from itertools import combinations
+from pprint import pprint
 
 '''
 Strategy
 
-- Remove duplicates from original word list of length k if exist and use k' words
-- Now, we need to calculate k'! permutations of word to find shortest string
-- Since 1 <= k' <= 15, max are 15! which is intractable
-- If we want to calculate a!, we can reuse results of (a-1)!
+- If word1 is substring of word2, then we can only consider word2
+- If we already selected N words, then solution is impacted only by (last word in selected N words, set of remained words)
+- (N-1) words in front do not affect solution
 '''
 
 
-# merge given 2 words and return list of shortest merged string
-# if w1w2 and w2w1 are same length, return both
-def merge_word(word1, word2):
-    min_len = min(len(word1), len(word2))
-    w12 = word1 + word2
-    w21 = word2 + word1
-    for i in reversed(range(min_len)):
-        if word1.endswith(word2[:i+1]):
-            w12 = word1 + word2[i+1:]
-            break
+'''
+merged(word1, word2) = diff(word1, word2) + word2
+E.g, word1 = 'abcde' and word2 = 'cdefg', then diff(word1, word2) = 'ab'
+'''
+def diff(word1, word2):
+    # since each word does not fully overlap each other, the maximum overlaped length equals to (length of shorter word)-1
+    for length in reversed(range(1, min(len(word1), len(word2)))):
+        if word2.startswith(word1[-length:]):
+            return word1[:-length]
 
-    for i in reversed(range(min_len)):
-        if word2.endswith(word1[:i+1]):
-            w21 = word2 + word1[i+1:]
-            break
+    # does not overlap at all
+    return word1
 
-    if len(w12) == len(w21):
-        return [w12, w21]
-    else:
-        return [min(w12, w21, key=len)]
+# remove substrings
+def remove_substrings(words):
+    words = set(words)
+    result = set(words)
+    # remove fully overlaped words
+    for word1 in words:
+        for word2 in words:
+            # skip two words are same
+            if word1 == word2:
+                continue
 
+            if word1.find(word2) != -1:
+                result.discard(word2)
+
+    return result
+
+
+def get_min_string(diff_cache, answer_cache, last, remained):
+    if remained == ():
+        return last
+
+    key = (last, remained)
+    if key in answer_cache:
+        return answer_cache[key]
+
+    candidates = []
+    _remained = set(remained)
+    for first in remained:
+        _remained.remove(first)
+        diff = diff_cache[(last, first)]
+        curr = diff_cache[(last, first)] + get_min_string(diff_cache, answer_cache, first, tuple(_remained))
+        candidates.append(curr)
+        _remained.add(first)
+
+    candidates.sort(key=len)
+    answer_cache[key] = candidates[0]
+    return answer_cache[key]
 
 def solution(words):
+    diff_cache = dict()     # cache for diff of 2 words
+    answer_cache = dict()   # cache for saving answer
+    words = remove_substrings(words)    # remove substrings
     digit = len(words)
-    cache = dict()
 
-    # init cache
-    for comb in combinations(range(digit), 1):
-        curr_word = words[comb[0]]
+    # fill diff_cache
+    for word1 in words:
+        for word2 in words:
+            if word1 == word2:
+                continue
 
-        cache[comb] = {curr_word}
+            key = (word1, word2)
+            diff_cache[key] = diff(word1, word2)
 
-    # fill cache
-    for i in range(2, digit+1):
-        for comb in combinations(range(digit), i):
-            results = set()
-            for j in range(i):
-                curr_word = words[comb[j]]
-                curr_comb = comb[:j] + comb[j+1:]
-                prev = cache[curr_comb] # cached result
-                next = [merge_word(curr_word, p) for p in prev]
-                next = set(chain(*next))
-                next = { x for x in next if len(x) == len(min(next, key=len)) } # extract shortest
-                results.update(next)
+    candidates = []
+    for word in words:
+        words.remove(word)
+        candidates.append(get_min_string(diff_cache, answer_cache, word, tuple(words)))
+        words.add(word)
 
-            results = { x for x in results if len(x) == len(min(next, key=len)) } # extract shortest
-            cache[comb] = results
-
-    key = tuple(range(digit))
-    result = cache[key].pop()
-    return result
+    candidates.sort(key=len)
+    return candidates[0]
 
 
 if __name__ == "__main__":
