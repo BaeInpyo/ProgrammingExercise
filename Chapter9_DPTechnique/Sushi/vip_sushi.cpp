d7708f70194f8a9677430cb2c209c603033fad94
BaeInpyo <dkas2@snu.ac.kr>
2019-10-05 18:22:12 +0900

    Implement iterator version but wrong answer

--- Chapter9_DPTechnique/Sushi/vip_sushi.cpp
+++ Chapter9_DPTechnique/Sushi/vip_sushi.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
 #define MAX 20
-#define MAX_BUDGET 21474836
+#define MAX_BUDGET 200
 
 using namespace std;
 
 int num_tests;
 int num_sushi;
 int budget;
-int min_price = 200;
+int min_price = MAX_BUDGET;
 int price[MAX];
 int prefer[MAX];
 int cache[MAX_BUDGET];
@@ -40,9 +40,32 @@ int choose_best(int remain_budget) {
   return preference;
 }
 
+int choose_best_iter() {
+  int max_prefer = 0;
+
+  for (int curr_price = 1; curr_price <= budget; ++curr_price) {
+    int &curr_prefer = cache[curr_price % MAX_BUDGET];
+    for (int i = 0; i < num_sushi; ++i) {
+      int prev_price = curr_price - price[i];
+      if (prev_price >= 0) {
+        int temp_prefer = cache[prev_price % MAX_BUDGET] + prefer[i];
+        if (temp_prefer > curr_prefer) {
+          curr_prefer = temp_prefer;
+        }
+      }
+    }
+    if (curr_prefer > max_prefer) {
+      max_prefer = curr_prefer;
+    }
+  }
+
+  return max_prefer;
+}
+
 void solution() {
   init_cache();
-  cout << choose_best(budget) << endl;
+  //cout << choose_best(budget) << endl;
+  cout << choose_best_iter() << endl;
 }
 
 int main () {
