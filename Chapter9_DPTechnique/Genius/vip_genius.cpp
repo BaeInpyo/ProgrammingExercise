566172a3047a84adc2dce4809a3ffc50052308ee
BaeInpyo <dkas2@snu.ac.kr>
2019-10-09 01:51:17 +0900

    Pass, but ugly code

--- Chapter9_DPTechnique/Genius/vip_genius.cpp
+++ Chapter9_DPTechnique/Genius/vip_genius.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 #define MAX_MUSIC 50
 #define MAX_FAVOR 10
@@ -27,45 +28,48 @@ void init_cache() {
 void solution() {
   init_cache();
 
-  double base_prob = 1 / (double)num_music;
-  for (int time = 1; time <= num_min; ++time) {
+  double base_prob = 1.0;
+  double no_prob = 0.0;
+  // Base case
+  if (num_min < music_len[0]) {
+    for (int favorite_music : favorite) {
+      if (favorite_music == 0) cout << base_prob << " ";
+      else cout << no_prob << " ";
+    }
+    cout << endl;
+    return;
+  }
+
+  cache[music_len[0]][0] = 1;
+
+  for (int time = music_len[0]+1; time <= num_min; ++time) {
     for (int curr_music = 0; curr_music < num_music; ++curr_music) {
       int curr_music_len = music_len[curr_music];
       int music_end = time - curr_music_len;
-      if (music_end < 0) continue;
-      if (music_end == 0) cache[curr_music_len][curr_music] = base_prob;
+      if (music_end < music_len[0]) continue;
       else {
         music_end = music_end % MAX_CACHE;
+        double curr_prob = 0;
         for (int prev_music = 0; prev_music < num_music; ++prev_music) {
           double prev_prob = cache[music_end][prev_music];
-          double curr_prob = prev_prob * prob[prev_music][curr_music];
-          cache[time % MAX_CACHE][curr_music] = curr_prob;
+          curr_prob += prev_prob * prob[prev_music][curr_music];
         }
+        cache[time % MAX_CACHE][curr_music] = curr_prob;
       }
     }
   } 
 
-  int time = num_min % MAX_CACHE;
   for (int i = 0; i < num_favorite; ++i) {
     int favorite_music = favorite[i];
     int favorite_music_len = music_len[favorite_music];
     double curr_prob = 0;
-    bool is_base_added = false;
-    for (int prev_music = 0; prev_music < num_music; ++prev_music) {
-      for (int music_end = num_min; music_end > num_min - favorite_music_len && music_end >= 0; --music_end) {
+    for (int music_end = num_min; music_end > num_min - favorite_music_len && music_end >= 0; --music_end) {
+      for (int prev_music = 0; prev_music < num_music; ++prev_music) {
         double prev_prob = cache[music_end % MAX_CACHE][prev_music];
-        if (!is_base_added && prev_prob == 0) {
-          curr_prob += base_prob;
-          is_base_added = true;
-          break;
-        }
-        else {
-          curr_prob += prev_prob * prob[prev_music][favorite_music];
-        }
+        curr_prob += prev_prob * prob[prev_music][favorite_music];
       }
-    } 
-
-    cout << curr_prob << " ";
+    }
+    cout << setprecision(8) << curr_prob << " ";
   }
   cout << endl;
 }
