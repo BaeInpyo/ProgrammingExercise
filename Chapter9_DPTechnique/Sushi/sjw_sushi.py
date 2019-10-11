3f1a67ceae5b2fce9ef16717ec7e4ee45beae8f2
bmy4415 <bmy4415@naver.com>
2019-10-11 18:30:00 +0900

    sushi temp status

--- Chapter9_DPTechnique/Sushi/sjw_sushi.py
+++ Chapter9_DPTechnique/Sushi/sjw_sushi.py
@@ -1,8 +1,47 @@
 import sys
 import os
+from fractions import gcd
+from itertools import reduce
 
 
-def solution(sushi, budget):
+def solution_reduce_budget(sushi, budget):
+    # reduce total budget to small enough
+    # this can lead to fewer iteration
+    def lcm(a, b):
+        return int(a * b / gcd(a, b))
+
+    sushi.sort(key=lambda e: e['value'] / e['price'])   # sort by value per price
+    sushi_cleaned = []  # remove unnecessary sushi
+
+    # if s2['price'] is multiple of s1['price'] and s1 has better (value / price)
+    # then s2 is unnecessary because s2 can be replaced by s1
+    for i in range(len(sushi)):
+        curr_price = sushi[i]['price']
+        if all([e['price'] % curr_price for e in sushi_cleaned]):
+            sushi_cleaned.append(sushi[i])
+
+    lcm_all_sushi = reduce(lambda x, y: lcm(x, y), sushi_cleaned)
+    value = 0   # sum of values
+    if budget > lcm_all_sushi:
+        count = (budget - lcm_all_sushi) // sushi_cleaned[0]['price']   # pick best (value / price) AMAP
+        budget -= count * sushi_cleaned[0]['price']
+        value += count * sushi_cleaned[0]['value']
+
+    # fill cache on reduced budget
+    cache = [0] * (len(budget) + 1)
+    for i in range(1, budget + 1):
+        candidates = []
+        for s in sushi_cleaned:
+            if budget >= s['price']:
+                curr = s['value'] + cache[budget - s['price']]
+                candidates.append(curr)
+
+        cache[i] = max(candidates)
+
+    return value + cache[-1]
+
+
+def solution_minimal_window(sushi, budget):
     # below are similar with knapsack problem
     # sliding cache
     cache = [0] * 201
@@ -38,16 +77,16 @@ if __name__ == '__main__':
     c = int(sys.stdin.readline().strip())
     answers = []
     for _ in range(c):
-        n, m = [int(x) for x in sys.stdin.readline().strip().split()]
+        n, m = [int(x) for x in sys.stdin.readline().strip().split()]   # read n, m
         sushi = []
         for _ in range(n):
-            price, value = [int(x) for x in sys.stdin.readline().strip().split()]
+            price, value = [int(x) for x in sys.stdin.readline().strip().split()]   # read price and value
             sushi.append((int(price/100), value))
 
         sushi = list(set(sushi))    # keep unique (price, value)
         sushi = [{'price': x[0], 'value': x[1]} for x in sushi]    # convert to list of dict
 
-        answer = solution(sushi, int(m/100))
+        answer = solution_reduce_budget(sushi, int(m/100))
         answers.append(answer)
 
     for a in answers:
