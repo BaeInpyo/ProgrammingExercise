2dbfc8d02e822071d71b24daa91d156e336dade2
ene5135 <ene5135@naver.com>
2019-12-01 23:36:02 +0900

    후보들을 처음에 컷해놓고 시작하도록 수정, 통과

--- Chapter11_CombinationalSearch/Kakuro2/ygg_kakuro2.py
+++ Chapter11_CombinationalSearch/Kakuro2/ygg_kakuro2.py
@@ -2,86 +2,110 @@ import sys
 
 answer = []
 
-def solution(board, horiHintDict, vertiHintDict, idx):
+def solution(board, horiHintDict, vertiHintDict, boardIdx):
 
-    if idx >= (len(board)-1)**2:
+    if boardIdx >= (len(board)-1)**2:
         return True
 
-    currY, currX = idx//(len(board)-1)+1, idx%(len(board)-1)+1 # 최외곽 제외 인덱싱
+    currY, currX = boardIdx//(len(board)-1)+1, boardIdx%(len(board)-1)+1 # 최외곽 제외 인덱싱
 
     if board[currY][currX] == 0:
-        return solution(board, horiHintDict, vertiHintDict, idx+1)
+        return solution(board, horiHintDict, vertiHintDict, boardIdx+1)
 
-    for x, val, candiDict in horiHintDict[currY]: 
-        if x > currX: # 힌트가 있는자리가 현재 x보다 크면 break
+    for idx, currHint in enumerate(horiHintDict[currY]): 
+        if currHint[0] > currX: # 힌트가 있는자리가 현재 x보다 크면 break
             break
-        horiHint = (x, val, candiDict) # 현재 위치에서 가장 가까운 수평힌트
-    for y, val, candiDict in vertiHintDict[currX]: 
-        if y > currY: # 힌트가 있는자리가 현재 y보다 크면 break
+        horiHint = horiHintDict[currY][idx]
+
+    for idx, currHint in enumerate(vertiHintDict[currX]): 
+        if currHint[0] > currY: # 힌트가 있는자리가 현재 y보다 크면 break
             break
-        vertiHint = (y, val, candiDict) # 현재 위치에서 가장 가까운 수직힌트
+        vertiHint = vertiHintDict[currX][idx]
 
-    horiCandi = horiHint[2]
-    vertiCandi = vertiHint[2]
+        #vertiHint = (y, val, candiDict, colWhite) # 현재 위치에서 가장 가까운 수직힌트
 
-    filledCount = 0
-    filledSumHori = 0
-    for x in range(horiHint[0]+1, len(board)):
-        if board[currY][x] == 0:
-            break
-        if answer[currY][x] != 0:
-            filledCount += 1
-            filledSumHori += answer[currY][x]
-        rightMostWhite = x
-    horiNumOfWhite = rightMostWhite - horiHint[0] - filledCount
-
-    if sum(horiCandi[:horiNumOfWhite])+filledSumHori > horiHint[1] or sum(horiCandi[-horiNumOfWhite:])+filledSumHori < horiHint[1]: # 제일큰거 n개나 제일작은거 n개 선택해도 만족못하면
-        return False
-
-    filledCount = 0
-    filledSumVerti = 0
-    for y in range(vertiHint[0]+1, len(board)):
-        if board[y][currX] == 0:
-            break
-        if answer[y][currX] != 0:
-            filledCount += 1
-            filledSumVerti += answer[y][currX]
-        bottomMostWhite = y
-    vertiNumOfWhite = bottomMostWhite - vertiHint[0] - filledCount
-
-    if sum(vertiCandi[:vertiNumOfWhite])+filledSumVerti > vertiHint[1] or sum(vertiCandi[-vertiNumOfWhite:])+filledSumVerti < vertiHint[1]: # 제일큰거 n개나 제일작은거 n개 선택해도 만족못하면
-        return False
+    horiCandi = horiHint[3]
+    vertiCandi = vertiHint[3]
 
     possible = set(horiCandi) & set(vertiCandi)
     if len(possible) == 0: # 선택가능한 경우가 없을때
         return False
 
+    maxCandi = min(getMinCandi(horiHint[1], horiHint[2]), getMinCandi(vertiHint[1], vertiHint[2]))
+    minCandi = max(getMaxCandi(horiHint[1], horiHint[2]), getMaxCandi(vertiHint[1], vertiHint[2]))
+
     # 다 통과했으면 이제 본론
     for candi in possible:
-        if horiNumOfWhite == 1 and filledSumHori+candi != horiHint[1]: # 마지막 자리면
+        if candi > maxCandi or candi < minCandi:
             continue
-        if vertiNumOfWhite == 1 and filledSumVerti+candi != vertiHint[1]: # 마지막 자리면
-            continue
-        if candi >= horiHint[1] or candi >= vertiHint[1]: # 고른놈이 조건보다 더 크면
-            continue
-
         answer[currY][currX] = candi
-        horiIdx = horiCandi.index(candi)
-        horiCandi.pop(horiIdx)
-        vertiIdx = vertiCandi.index(candi)
-        vertiCandi.pop(vertiIdx)
-        if solution(board, horiHintDict, vertiHintDict, idx+1):
+        horiHint[1] -= candi 
+        horiHint[2] -= 1
+        vertiHint[1] -= candi 
+        vertiHint[2] -= 1
+        horiCandiIdx = horiCandi.index(candi)
+        vertiCandiIdx = vertiCandi.index(candi)
+        horiCandi.pop(horiCandiIdx)
+        vertiCandi.pop(vertiCandiIdx)
+        if solution(board, horiHintDict, vertiHintDict, boardIdx+1):
             return True
         # 되돌리기
         answer[currY][currX] = 0
-        horiCandi.insert(horiIdx, candi)
-        vertiCandi.insert(vertiIdx, candi)
+        horiCandi.insert(horiCandiIdx, candi)
+        vertiCandi.insert(vertiCandiIdx, candi)
+        horiHint[1] += candi 
+        horiHint[2] += 1
+        vertiHint[1] += candi 
+        vertiHint[2] += 1
 
     return False
 
     
-
+def getNumOfWhite(y, x, t, N, board):
+    count = 0
+    if t == 0:
+        for i in range(x+1, N):
+            if board[y][i] == 0:
+                break
+            count += 1
+    else:
+        for i in range(y+1, N):
+            if board[i][x] == 0:
+                break
+            count += 1
+    return count
     
+
+def getMinCandi(val, numOfWhite):
+    if numOfWhite == 1:
+        return val
+    minLimit = numOfWhite
+    minBase = sum(range(1,numOfWhite))
+    for i in range(numOfWhite+1,10):
+        if minBase + i > val:
+            break
+        minLimit = i
+    return minLimit
+
+def getMaxCandi(val, numOfWhite):
+    if numOfWhite == 1:
+        return val
+    maxLimit = 10-numOfWhite
+    maxBase = sum(range(11-numOfWhite,10))
+    for i in range(1, maxLimit+1):
+        if maxBase + i >= val:
+            return i
+
+
+def getCandi(val, numOfWhite):
+    if numOfWhite == 1:
+        return [val]
+
+    minLimit = getMinCandi(val, numOfWhite)
+    maxLimit = getMaxCandi(val, numOfWhite)
+
+    return list(range(maxLimit, minLimit+1))
+
 if __name__ == '__main__':
     #sys.stdin = open('input.in', 'r')
     C = int(sys.stdin.readline().rstrip())
@@ -94,10 +118,11 @@ if __name__ == '__main__':
         vertiHintDict = {}
         for _ in range(Q):
             y, x, t, val = list(map(int, sys.stdin.readline().rstrip().split()))
+            numOfWhite = getNumOfWhite(y-1, x-1, t, N, board)
             if t == 0:
-                horiHintDict[y-1] = sorted(horiHintDict.get(y-1, [])+[(x-1, val, [1,2,3,4,5,6,7,8,9])],key=lambda x: x[0])
+                horiHintDict[y-1] = sorted(horiHintDict.get(y-1, [])+[[x-1, val, numOfWhite, getCandi(val, numOfWhite)]],key=lambda x: x[0])
             else:
-                vertiHintDict[x-1] = sorted(vertiHintDict.get(x-1, [])+[(y-1, val, [1,2,3,4,5,6,7,8,9])],key=lambda x: x[0])
+                vertiHintDict[x-1] = sorted(vertiHintDict.get(x-1, [])+[[y-1, val, numOfWhite, getCandi(val, numOfWhite)]],key=lambda x: x[0])
         
 
         solution(board, horiHintDict, vertiHintDict, 0)
