088c032ea17ffec17cc394f47c64fcb27fe2e29b
bmy4415 <bmy4415@naver.com>
2019-12-07 19:16:45 +0900

    sjw arctic solved

--- Chapter12_Decision/Arctic/sjw_arctic.py
+++ Chapter12_Decision/Arctic/sjw_arctic.py
@@ -5,39 +5,60 @@ import math
 abs_path = os.path.abspath(os.path.dirname(__file__))
 sys.stdin = open(os.path.join(abs_path, 'input.txt'), 'r')
 
+'''
+Summary of decision method
+1. Define range of lower and upper bound of optimal value
+2. Define function which takes value as argument and test
+   if either [vaule, upperbound] or [lowerbound, value] satisfies condition
+3. Find optimal value in range [lowerbound, upperbound] using function defined in 2. and binary search
+
+In this Arctic problem, range is [0, 1000 * sqrt(2)] which is min/max value of distance between 2 points
+canCommunicate is function for Summary 2.
+'''
+
+DIST = None # DIST[i][j] = distance between points[i] and points[j]
+
+'''
+Check if given radius will connect all points
+time complexity: O(n^2)
+'''
 def canCommunicate(points, radius):
+    N = len(points)
+    visited = [0] * N
+    queue = []  # index of points
+    queue.append(0)
+    visited[0] = 1
+
+    while queue:
+        curr = queue.pop()
+        for i in range(N):
+            if visited[i] == 0 and DIST[curr][i] <= radius:
+                visited[i] = 1
+                queue.append(i)
+
+    return all(visited)
+
+'''
+We can define function get_distance(p1, p2) but this will be called too much.
+So make matrix for reducing computation
+'''
+def init_dist(points):
     def distance(p1, p2):
         x1, y1 = p1
         x2, y2 = p2
         return math.sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2))
-
-    def reachable(remained, selected):
-        for p1 in remained:
-            for p2 in selected:
-                if distance(p1, p2) <= radius:
-                    return p1
-
-        return None
-
-    remained = points.copy()
-    selected = set()
-    selected.add(remained.pop())
-
-    while remained:
-        point = reachable(remained, selected)
-        if point:
-            remained.remove(point)
-            selected.add(point)
-        else:
-            break
-
-    return len(remained) == 0
-
-    return len(remained) == 0
+    global DIST
+    N = len(points)
+    DIST = [[0] * N for _ in range(N)]  # N x N 2d matrix
+    for i in range(N):
+        for j in range(N):
+            DIST[i][j] = distance(points[i], points[j])
+    return
 
 def solution(points):
+    init_dist(points)   # init DIST matrix
     low, high = 0, 1000 * math.sqrt(2)
-    for _ in range(20):
+    for _ in range(30):
         mid = (low + high) / 2
         if canCommunicate(points, mid):
             high = mid
@@ -45,19 +66,19 @@ def solution(points):
             low = mid
 
     answer = round(high, 2)
-    print(answer)
+    print("{0:0.2f}".format(answer))
 
 def read_input():
     N = int(sys.stdin.readline().strip())
-    points = set()
+    points = []
     for _ in range(N):
         x, y = tuple(float(x) for x in sys.stdin.readline().strip().split())
-        points.add((x, y))
+        points.append((x, y))
 
     return points
 
 if __name__ == '__main__':
     T = int(sys.stdin.readline().strip())
     for _ in range(T):
-        points = read_input() # read input
+        points = read_input()
         solution(points)
