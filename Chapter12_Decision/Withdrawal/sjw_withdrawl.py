2c1871ad8c041b9d6eeb41c60773dcb9977ba4d9
bmy4415 <bmy4415@naver.com>
2020-01-04 22:13:45 +0900

    sjw - withdrawl O(N^2) but time exceed

--- Chapter12_Decision/Withdrawal/sjw_withdrawl.py
+++ Chapter12_Decision/Withdrawal/sjw_withdrawl.py
@@ -7,16 +7,29 @@ sys.stdin = open(os.path.join(abs_path, "input.txt"), "r")
 
 def solution(n, k, r, c):
     sum_r, sum_c = 0, 0
-    remained = set(zip(r, c))
+    selected_index = set()
     for _ in range(k):
         # find (ri,ci) that makes sum_r / sum_c minumum
-        candidates = [(sum_r + ri, sum_c + ci) for (ri, ci) in remained]
-        min_pair = min(candidates, key=lambda x: x[0] / x[1])
-        ri = min_pair[0] - sum_r
-        ci = min_pair[1] - sum_c
-        remained.remove((ri, ci))
-        sum_r += ri
-        sum_c += ci
+        min_sum_r, min_sum_c, min_idx = sum_r, sum_c, -1
+        for idx in range(n):
+            if idx in selected_index:
+                continue
+
+            if min_idx == -1:
+                min_sum_r += r[idx]
+                min_sum_c += c[idx]
+                min_idx = idx
+
+            else:
+                # the smaller one found
+                if (sum_r + r[idx]) / (sum_c + c[idx]) < min_sum_r / min_sum_c:
+                    min_sum_r = sum_r + r[idx]
+                    min_sum_c = sum_c + c[idx]
+                    min_idx = idx
+
+        sum_r = min_sum_r
+        sum_c = min_sum_c
+        selected_index.add(min_idx)
 
     answer = round(sum_r / sum_c, 10)
     return answer
