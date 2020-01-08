24b82039534cb4adf0f068018d24c9cc3746a832
BaeInpyo <dkas2@snu.ac.kr>
2020-01-09 00:38:29 +0900

    input 처리

---
+++ Chapter14_NumberTheory/Pass486/vip_pass486.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int num_tests;
+int num_divisor;
+int lo;
+int hi;
+
+void solution() {
+  
+}
+
+int main() {
+  cin.sync_with_stdio(false);
+
+  cin >> num_tests;
+
+  while (--num_tests >= 0) {
+    cin >> num_divisor;
+    cin >> lo;
+    cin >> hi;
+
+    solution();
+  }
+
+  return 0;
+}
