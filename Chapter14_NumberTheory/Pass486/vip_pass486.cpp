61e8898c8491b387f05ed23daf048b2e2f61ecf3
BaeInpyo <dkas2@snu.ac.kr>
2020-01-09 01:22:11 +0900

    timeout

--- Chapter14_NumberTheory/Pass486/vip_pass486.cpp
+++ Chapter14_NumberTheory/Pass486/vip_pass486.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -6,9 +7,42 @@ int num_tests;
 int num_divisor;
 int lo;
 int hi;
+int num_divisor_in_number[1000000];
+//int min_factor[10000000];
+
+void initialize() {
+  for (int i = 0; i < 1000000; ++i) {
+    num_divisor_in_number[i] = 0;
+  }
+}
+
+void calculate_divisors() {
+  for (int number = lo; number <= hi; ++number) {
+    int n_sqrt = sqrt(number);
+    for (int i = 1; i <= n_sqrt; ++i) {
+      if (number % i == 0) {
+        num_divisor_in_number[number - lo] += 2;
+      }
+    }
+    if (n_sqrt * n_sqrt == number) {
+      --num_divisor_in_number[number - lo];
+    }
+  }
+}
 
 void solution() {
+  initialize();
+  calculate_divisors();
+
+  int count = 0;
+  for (int number = 0; number < hi - lo + 1; ++number) {
+    if (num_divisor_in_number[number] == num_divisor) {
+      ++count;
+    }
+  }
   
+  cout << count << endl;
+  return;
 }
 
 int main() {
