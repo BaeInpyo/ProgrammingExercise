39097c4ea3badeca7806172b8952f133b8a71c25
BaeInpyo <dkas2@snu.ac.kr>
2020-01-09 01:58:09 +0900

    pass but slightly long time...

--- Chapter14_NumberTheory/Pass486/vip_pass486.cpp
+++ Chapter14_NumberTheory/Pass486/vip_pass486.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cmath>
+#include <unordered_map>
+
+#define MAX 10000000
 
 using namespace std;
 
@@ -7,37 +10,60 @@ int num_tests;
 int num_divisor;
 int lo;
 int hi;
-int num_divisor_in_number[1000000];
-//int min_factor[10000000];
+int min_factor[MAX + 1];
+int num_divisors[MAX + 1];
 
 void initialize() {
-  for (int i = 0; i < 1000000; ++i) {
-    num_divisor_in_number[i] = 0;
+  for (int i = 0; i <= MAX; ++i) {
+    num_divisors[i] = 0;
   }
 }
 
-void calculate_divisors() {
-  for (int number = lo; number <= hi; ++number) {
-    int n_sqrt = sqrt(number);
-    for (int i = 1; i <= n_sqrt; ++i) {
-      if (number % i == 0) {
-        num_divisor_in_number[number - lo] += 2;
+void calculate_min_factor() {
+  int n = MAX;
+  int n_square = sqrt(MAX);
+
+  for (int i = 1; i <= MAX; ++i) {
+    min_factor[i] = i;
+  }
+  for (int i = 2; i <= n_square; ++i) {
+    if (min_factor[i] == i) {
+      for (int j = i*i; j <= MAX; j += i) {
+        if (min_factor[j] == j) {
+          min_factor[j] = i;
+        }
       }
-    }
-    if (n_sqrt * n_sqrt == number) {
-      --num_divisor_in_number[number - lo];
     }
   }
 }
 
 void solution() {
-  initialize();
-  calculate_divisors();
-
   int count = 0;
-  for (int number = 0; number < hi - lo + 1; ++number) {
-    if (num_divisor_in_number[number] == num_divisor) {
-      ++count;
+  for (int number = lo; number <= hi; ++number) {
+    int n = number;
+    int &num_divisor_in_number = num_divisors[number];
+
+    if (num_divisor_in_number == 0) {
+      unordered_map<int, int> primes;
+      while (n > 1) {
+        int factor = min_factor[n];
+        auto it = primes.find(factor);
+        if (it != primes.end()) {
+          primes[factor] += 1;
+        } else {
+          primes.insert(make_pair(factor, 1));
+        }
+        n /= min_factor[n];
+      }
+      
+      num_divisor_in_number = 1;
+      for (auto prime : primes) {
+        num_divisor_in_number *= (prime.second + 1);
+      }
+    }
+
+    if (num_divisor_in_number == num_divisor) {
+      count++;
     }
   }
   
@@ -50,6 +76,8 @@ int main() {
 
   cin >> num_tests;
 
+  initialize();
+  calculate_min_factor();
   while (--num_tests >= 0) {
     cin >> num_divisor;
     cin >> lo;
