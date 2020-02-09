e47f0dcd227b21606d3b8755f3a622019dce713e
ene5135 <ene5135@naver.com>
2020-02-09 23:38:16 +0900

    시간측정 결과 추가

--- Chapter19_QueueStackDeque/Ites/ygg_ites.py
+++ Chapter19_QueueStackDeque/Ites/ygg_ites.py
@@ -1,6 +1,16 @@
 import sys
 INT_MAX = 2**32
 from collections import deque
+import time
+
+class logger():
+    def __init__(self, func):
+        self.func = func
+    def __call__(self, *args, **kwargs):
+        start = time.time()
+        result = self.func(*args, **kwargs)
+        print(f"[{self.func.__name__}] Elapsed time: {time.time()-start}")
+        return result
 
 def signal_generator(N):
     curr = 1983
@@ -8,6 +18,7 @@ def signal_generator(N):
         yield curr%10000 + 1
         curr = (curr * 214013 + 2531011) % INT_MAX
 
+@logger
 def solution(K, N):
     count = 0
     signals = signal_generator(N)
@@ -31,6 +42,9 @@ def solution(K, N):
 
     return count
     
+
+
+        
 if __name__ == '__main__':
     sys.stdin = open('input.in', 'r')
     C = int(sys.stdin.readline().rstrip())
@@ -38,4 +52,11 @@ if __name__ == '__main__':
         K, N = [int(x) for x in sys.stdin.readline().rstrip().split()]
         print(solution(K, N))
 
-
+""" results
+[solution] Elapsed time: 0.0
+1
+[solution] Elapsed time: 0.006003618240356445
+4
+[solution] Elapsed time: 6.845235347747803
+1049
+"""
