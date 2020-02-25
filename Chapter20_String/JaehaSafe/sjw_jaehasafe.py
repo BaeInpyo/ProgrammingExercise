0737192c2fef19dedb1dfa6edb75a52dbf1b25ff
bmy4415 <bmy4415@naver.com>
2020-02-26 02:25:35 +0900

    sjw jaehasafe - sovled

--- Chapter20_String/JaehaSafe/sjw_jaehasafe.py
+++ Chapter20_String/JaehaSafe/sjw_jaehasafe.py
@@ -1,46 +1,43 @@
 import sys
 import os
-from collections import defaultdict
+
 
 abs_dir = os.path.dirname(os.path.abspath(__file__))
 sys.stdin = open(os.path.join(abs_dir, "input.txt"), "r")
 
 
 def solution(n, states):
-    cache = {}  # key(state1, state2), value: number of left rotates
 
-    m = len(states[0])
-    result = 0  # number of counts start from left rotate
+    answer = 0
     for idx in range(n):
-        src_state = states[idx]
-        dst_state = states[idx + 1]
+        src = states[idx]
+        dst = states[idx + 1]
 
-        # left rotate
-        for count in range(1, m):
-            intermid_state = src_state[count:] + src_state[:count]
-            key1 = (src_state, intermid_state)
-            key2 = (intermid_state, src_state)
-            cache[key1] = min(cache.get(key1, m), count)
-            cache[key2] = min(cache.get(key2, m), m - count)
-            if intermid_state == dst_state:
-                if idx % 2 == 0:
-                    # print("left ", src_state, "=>", dst_state, ": ", cache[key1])
-                    result += cache[key1]
-                else:
-                    # print("right ", src_state, "=>", dst_state, ": ", m - cache[key1])
-                    result += m - cache[key1]
+        if idx % 2 == 0:
+            # clock wise
+            string = dst * 2
+            pattern = src
+        else:
+            # counter-clock wise
+            string = src * 2
+            pattern = dst
 
-    return str(n * m - result)
+        for jdx in range(1, len(string)):
+            if string[jdx:jdx+len(pattern)] == pattern:
+                answer += jdx
+                break
+
+    print(answer)
+    return
+
 
 if __name__ == "__main__":
-    C = int(sys.stdin.readline().strip())
-    answers = [None] * C
-    for idx in range(C):
-        N = int(sys.stdin.readline().strip())
-        states = [None] * (N + 1)
-        for jdx in range(N + 1):
-            states[jdx] = sys.stdin.readline().strip()
+	C = int(input())
+	for _ in range(C):
+		N = int(input())
+		states = [None] * (N + 1)
+		for idx in range(N + 1):
+			state = input()
+			states[idx] = state
 
-        answers[idx] = solution(N, states)
-
-    sys.stdout.write("\n".join(answers) + "\n")
+		solution(N, states)
