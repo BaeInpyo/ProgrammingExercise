d11bb5cfcfddc8f5193ad2b33e692295856b9b66
Kim JunHwan <kimjh.snu@gmail.com>
2020-03-03 00:09:20 +0900

    452ms

--- Chapter22_BST/Insertion/kjh_insertion.py
+++ Chapter22_BST/Insertion/kjh_insertion.py
@@ -1,9 +1,6 @@
 for _ in range(int(input())):
-    array = list(range(int(input())))
-    for i, shifts in enumerate(map(int, input().split())):
-        for j in range(i, i-shifts, -1):
-            array[j], array[j-1] = array[j-1], array[j]
-    ret = [0] * len(array)
-    for i, val in enumerate(array):
-        ret[val] = i+1
-    print(*ret)
+    arr = list(range(1, int(input())+1))
+    ret = []
+    for i in reversed(list(map(int, input().split()))):
+        ret.append(arr.pop(-i-1))
+    print(*reversed(ret))
