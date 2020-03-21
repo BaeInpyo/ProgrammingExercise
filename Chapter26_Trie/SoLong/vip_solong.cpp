1f7ebc395ba80e4ffce8e67973706a96b18af352
BaeInpyo <dkas2@snu.ac.kr>
2020-03-21 16:10:59 +0900

    skeleton

---
+++ Chapter26_Trie/SoLong/vip_solong.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int num_tests;
+
+void solution() {
+
+}
+
+int main() {
+  cin.sync_with_stdio(false);
+  cin >> num_tests;
+  while (--num_tests >= 0) {
+    solution();
+  }
+  return 0;
+}
