13db615de422366b99b979cfed2b5b14a05eb48d
BaeInpyo <dkas2@snu.ac.kr>
2020-03-28 01:51:55 +0900

    skeleton

---
+++ Chapter28_DFS/Gallery/vip_gallery.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int num_gallery;
+int num_hall;
+
+void solution() {}
+
+int main() {
+  cin.sync_with_stdio(false);
+  int c; cin >> c;
+  while (c--) {
+    cin >> num_gallery >> num_hall;
+    for (int i = 0; i < num_hall; ++i) {
+      int a; int b;
+      cin >> a >> b;
+    }
+    solution();
+  }
+  return 0;
+}
