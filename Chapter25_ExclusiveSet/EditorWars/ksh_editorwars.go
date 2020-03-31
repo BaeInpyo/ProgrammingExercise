9f02bd4f5d4396f84d2e1407708f65c5c82ab2d4
shinhwi.kim <kimsh0205@daum.net>
2020-03-31 19:37:54 +0900

    inf loop

--- Chapter25_ExclusiveSet/EditorWars/ksh_editorwars.go
+++ Chapter25_ExclusiveSet/EditorWars/ksh_editorwars.go
@@ -5,12 +5,24 @@ import (
 	"os"
 )
 
+type inputType struct {
+	command int
+	a       int
+	b       int
+}
+
+const (
+	ackType = 0
+	disType = 1
+)
+
 var (
 	belongsTo [10000]int
 	dislike   [10000]int
 	height    [10000]int
 	weight    [10000]int
 	n, m      int
+	input     [100001]inputType
 )
 
 func initialize() {
@@ -29,36 +41,121 @@ func find(a int) int {
 	return a
 }
 func union(a int, b int) int {
+	if a == -1 || b == -1 {
+		if a < b {
+			return a
+		}
+		return b
+	}
 	pa := find(a)
 	pb := find(b)
 	if pa == pb {
 		return pa
 	}
 	if height[pa] < height[pb] {
-		belongsTo[pa] = pb
-		weight[pb] += weight[pa]
-		return pb
-	} else if height[pa] > height[pb] {
-		belongsTo[pb] = pa
-		weight[pa] += weight[pb]
-		return pa
-	} else {
-		belongsTo[pb] = pa
-		weight[pa] += weight[pb]
+		temp := pa
+		pa = pb
+		pb = temp
+	} // pa >= pb
+
+	if height[pa] == height[pb] {
 		height[pa]++
-		return pa
 	}
+	belongsTo[pb] = pa
+	weight[pa] += weight[pb]
+	return pa
+
 }
 
-func checkUnion(a int, b int) bool {
+func ack(a int, b int) (int, bool) {
 	// a가 싫어하는 집단이 b집단인지 봐야함 -> 실패
 	// b가 싫어하는 집단이 a집단인지 봐야함 -> 실패
 	// a,b를 union해준다.
 	// 그리고 리턴받은 값(합쳐진 부모)의 dislike를 바꿔준다.
 	// a,b가 서로 싫어하는 집단이 같은 집단으로 합칠수있는지 또 check_union해야함
+	pa := find(a)
+	pb := find(b)
 
+	if dislike[pa] == pb {
+		return -1, false
+	} // 적대관계이다. 어차피 쌍방이므로 한쪽만 보면될듯...
+
+	pp := union(pa, pb)
+	dd, ok := ack(pa, pb)
+	if ok == false {
+		return -1, false
+	}
+	dislike[pp] = dd
+	if dd != -1 {
+		dislike[dd] = pp
+	}
+	return pp, false
 }
 
+func dis(a int, b int) bool {
+	// a가 싫어하는 집단이 b집단인지 봐야함 -> 실패
+	// b가 싫어하는 집단이 a집단인지 봐야함 -> 실패
+	// a,b를 union해준다.
+	// 그리고 리턴받은 값(합쳐진 부모)의 dislike를 바꿔준다.
+	// a,b가 서로 싫어하는 집단이 같은 집단으로 합칠수있는지 또 check_union해야함
+	pa := find(a)
+	pb := find(b)
+	if pa == pb {
+		return false
+	}
+	// pa의 적이랑 pb랑 짝이고
+	// pb의 적이랑 pa랑 짝이다.
+	na, ok := ack(pa, dislike[pb])
+	if ok == false {
+		return false
+	}
+	nb, ok := ack(pb, dislike[pa])
+	if ok == false {
+		return false
+	}
+	dislike[na] = nb
+	dislike[nb] = na
+	return true
+}
+func solution() int {
+	var idx int
+	for idx = 1; idx <= m; idx++ {
+		var ok bool
+		if input[idx].command == ackType {
+			_, ok = ack(input[idx].a, input[idx].b)
+		} else {
+			ok = dis(input[idx].a, input[idx].b)
+		}
+		if ok == false {
+			break
+		}
+	}
+	return idx
+}
+
+func calcMax() int {
+	var result int = 0
+	for i := 0; i < n; i++ {
+		if find(i) == i { // 루트인 애들에 대해서만 보면 됨
+			enemy := dislike[i]
+			if enemy > i {
+				continue
+			}
+			myWeight := weight[i]
+			enemyWeight := 0
+			if enemy != -1 {
+				enemyWeight = weight[enemy]
+			}
+			if myWeight > enemyWeight {
+				result += myWeight
+			} else {
+				result += enemyWeight
+			}
+		}
+	}
+
+	return -1
+}
 func main() {
 	stdin, _ := os.OpenFile("input.txt", os.O_RDONLY, 0666)
 	os.Stdin = stdin
@@ -68,45 +165,21 @@ func main() {
 		fmt.Scanf("%d %d", &n, &m)
 		initialize()
 		var comment string
-		var a, b int
 		var idx int
 		for idx = 1; idx <= m; idx++ {
-			fmt.Scanf("%s %d %d", &comment, &a, &b)
+			fmt.Scanf("%s %d %d", &comment, &input[idx].a, &input[idx].b)
 			if comment == "ACK" {
-				// a,b랑 합쳐주면 되는데
-				checkUnion(a, b)
+				input[idx].command = ackType
 			} else {
-				// a, b가 적대관계이면.. 서로 부모의 dislike에 넣어주고..
-				// 만약에 같은 부모이면 충돌..
-				// 이미 pa집단에 싫어하는 모임이 있다면 check_union
-				// 이미 pb집단에 싫어하는 모임이 있다면 check_union
-				pa := find(a)
-				pb := find(b)
-				if pa == pb {
-					break
-				}
-				if dislike[pa] == -1 {
-					dislike[pa] = pb
-				} else {
-					if !checkUnion(dislike[pa], pb) {
-						break
-					}
-				}
-				if dislike[pb] == -1 {
-					dislike[pb] = pa
-				} else {
-					if !checkUnion(dislike[pb], pa) {
-						break
-					}
-				}
+				input[idx].command = disType
 			}
 		}
-		// if idx <= m {
-		// 	fmt.Printf("CONTRADICTION AT %d\n", idx)
-		// } else {
-		// 	fmt.Printf("MAX PARTY SIZE IS %d\n", calcMax())
-		// }
-
+		result := solution()
+		if result <= m {
+			fmt.Printf("CONTRADICTION AT %d\n", result)
+		} else {
+			fmt.Printf("MAX PARTY SIZE IS %d\n", calcMax())
+		}
 	}
 }
 
@@ -121,8 +194,10 @@ union할때 dislike도 같이 옮겨줘야할듯
 
 그렇게 같이 만들어주면... 뭘해야하지?
 1번부터 돌면서 짝이없는애들은 서로 배척하는집단중에서는...큰놈을
-이렇게하면 두번계산하게되는데.. 어떻게 한번만 계산하지
+상관없는 애들은 그냥 싹다 모아서
+이렇게해야하는데.. 서로 배척하는 집단을 가져왔을때 패런트가 더 큰경우에만 계산
 */
+
 /*
 MAX PARTY SIZE IS 3
 MAX PARTY SIZE IS 100
