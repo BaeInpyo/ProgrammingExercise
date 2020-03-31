a060350edf66ab62d12dfc36d0d592b8122ff776
BaeInpyo <dkas2@snu.ac.kr>
2020-03-31 16:04:57 +0900

    skeleton

---
+++ Chapter29_BFS/ChildrenDay/vip_childrenday.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+string d;
+int n, m;
+
+const string wrong_string = "IMPOSSOBLE";
+
+void solution() {}
+
+int main() {
+  cin.sync_with_stdio(false);
+  int c; cin >> c;
+  while (c--) {
+    cin >> d >> n >> m;
+    solution();
+  }
+  return 0;
+}
