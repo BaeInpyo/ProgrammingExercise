b99196f9546ce1ea592b6a95049129f202915212
bmy4415 <bmy4415@naver.com>
2020-04-20 17:35:23 +0900

    sjw gallery - solved

--- Chapter28_DFS/Gallery/sjw_gallery.py
+++ Chapter28_DFS/Gallery/sjw_gallery.py
@@ -1,37 +1,54 @@
+"""
+We can think of gallery as node and hollway as edge.
+We will install camera on nodes so that it's adjacent nodes are watched.
+To minimize number of installed cameras, we will do dfs from all nodes, then
+install camera only if there is child node that is not watched.
+We also have to consider a case that all children nodes are watched but current
+node is not watched. (See has_parent parameter in dfs())
+Below is helpful test cases
+4 3
+0 1
+2 3
+3 4
+
+9 8
+0 1
+1 2
+2 3
+3 4
+0 5
+5 6
+6 7
+7 8
+"""
+
 import os
 import sys
 
 abs_dir = os.path.dirname(os.path.abspath(__file__))
 sys.stdin = open(os.path.join(abs_dir, "input.txt"), "r")
 
-UNWATCHED = 0
-WATCHED = 1
-INSTALLED = 2
-
 
 def solution(g, h, hollways):
-    def dfs(here, adj, visited, watched):
-        """
-        start dfs from here
-        return number of installed camera
-        - find child
-        - if child is not covered, install here
-        """
+    def dfs(here, adj, visited, watched, has_parent):
+        """ start dfs from here then return number of installed camera """
         if here in visited: # here is already visited
             return 0
 
-        # print("visit:", here)
         visited.add(here)   # mark as visited
 
         children = set.difference(adj[here], visited)
-        ret = sum([dfs(child, adj, visited, watched) for child in children])
-        # ** below line is game changeer **
-        if set.difference(children, watched):
-        # if set.difference(adj[here], watched):
+        ret = sum([dfs(child, adj, visited, watched, True) \
+                   for child in children])
+
+        # install camera here if
+        # 1. There is unwatched child
+        # 2. here does not have parent and here is not watched
+        if set.difference(children, watched) or \
+           ((not has_parent) and (here not in watched)):
             ret += 1    # insert camera here
             watched.add(here)
             watched.update(adj[here])
-            # print("insert:", here)
         return ret
 
     adj = [set() for _ in range(g)]
@@ -40,14 +57,9 @@ def solution(g, h, hollways):
         adj[start].add(end)
         adj[end].add(start)
 
-    ret = 0
-    for i in range(g):
-        ret += dfs(i, adj, visited, watched)
-        if i not in watched:
-            ret += 1
-
+    ret = sum([dfs(i, adj, visited, watched, False) for i in range(g)])
     print(ret)
-    # print("------------------------------------------")
+
 
 if __name__ == "__main__":
     for _ in range(int(input())):
