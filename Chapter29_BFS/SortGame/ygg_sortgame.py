0b0492a57ffdf1b301677e2a77d4e0528cb60d77
ene5135 <ene5135@naver.com>
2020-04-26 02:51:52 +0900

    오늘의 교훈: 딕셔너리의 키는 리스트->튜플 말고 애초에 스트링을 사용하자.
    시간초과 범인 목록
    1. list to tuple operation(객체 할당 시간 오래걸리는것으로 추측)
    2. list concatenation(마찬가지)

--- Chapter29_BFS/SortGame/ygg_sortgame.py
+++ Chapter29_BFS/SortGame/ygg_sortgame.py
@@ -3,8 +3,8 @@ import collections
 distance_dict = {}
 
 def get_distance():
-    root_seq = [i+1 for i in range(8)]
-    distance_dict[tuple(root_seq)] = 0
+    root_seq = "12345678"
+    distance_dict[root_seq] = 0
     # 모든 경우에대해 다 뒤집어봄, 가장 distance가 낮은거부터 차례대로 확인 (재귀콜을 iterative 하게 수행, 맥스 재귀 깊이 에러떠서)
     call_stack = collections.deque([(root_seq, 1)])
     while call_stack:
@@ -15,28 +15,28 @@ def get_distance():
             for fr in range(0, 8-length):
                 to = fr + length
                 rev_seq = curr_seq[:fr] + curr_seq[fr:to+1][::-1] + curr_seq[to+1:]
-                rev_seq_key = tuple(rev_seq)
 
-                if distance_dict.get(rev_seq_key) != None: # 이미 확인한 수열이면 패스
+                if distance_dict.get(rev_seq) != None: # 이미 확인한 수열이면 패스
                     continue
 
-                distance_dict[rev_seq_key] = distance
+                distance_dict[rev_seq] = distance
                 call_stack.append((rev_seq, distance+1))
         
     return
 
 def seq_1_to_8(sequence):
     sorted_seq = sorted(sequence)
-    value_to_idx_dict = {value:i+1 for i, value in enumerate(sorted_seq)}
-    return [value_to_idx_dict[value] for value in sequence] + [i+1 for i in range(len(sequence), 8)]
+    value_to_idx_dict = {value:str(i+1) for i, value in enumerate(sorted_seq)}
+    return ''.join([value_to_idx_dict[value] for value in sequence] + [str(i+1) for i in range(len(sequence), 8)])
 
 if __name__ == '__main__':
     sys.stdin = open('input.in', 'r')
     C = int(sys.stdin.readline().rstrip())
     get_distance()
+
     for _ in range(C):
         N = int(sys.stdin.readline().rstrip())
         sequence = seq_1_to_8([int(x) for x in sys.stdin.readline().rstrip().split()])
-        print(distance_dict[tuple(sequence)])
+        print(distance_dict[sequence])
 
 
