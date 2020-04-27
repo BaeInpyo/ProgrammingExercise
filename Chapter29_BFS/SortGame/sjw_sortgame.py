f9aad930df6d5c3992df0890e26bc4245dbd9848
bmy4415 <bmy4415@naver.com>
2020-04-27 16:16:31 +0900

    add comment on sjw sortgame

--- Chapter29_BFS/SortGame/sjw_sortgame.py
+++ Chapter29_BFS/SortGame/sjw_sortgame.py
@@ -1,13 +1,18 @@
+"""
+중복이 없는 길이가 n인 string을 중간 순서를 바꾸어 만들 수 있는 모든 string의
+갯수는 n!이다. 이 n!개의 string을 node로 생각하고 중간의 순서를 뒤집는 것을 edge
+로 생각하여 임의의 시작점 "01234567"로부터 모든 경우(n!)의 순서 뒤집기 횟수를
+bfs search를 통하여 구하면 (DP에서의 cache를 채우는것과 유사함) 주어진 입력을
+sorted string으로 바꾸는데에 필요한 뒤집기 횟수를 바로 알 수 있다.
+"""
+
 import sys
 import os
 from collections import deque
 
-# freopen equivalent
-abs_dir = os.path.dirname(os.path.abspath(__file__))
-sys.stdin = open(os.path.join(abs_dir, "input.txt"), "r")
-
-
-
+# # freopen equivalent
+# abs_dir = os.path.dirname(os.path.abspath(__file__))
+# sys.stdin = open(os.path.join(abs_dir, "input.txt"), "r")
 
 def solution(n, sequence):
     idx = [x for x in range(n)]
@@ -15,7 +20,6 @@ def solution(n, sequence):
     idx.extend(range(n, 8))
     answer = cache["".join([str(x) for x in idx])]
     sys.stdout.write(str(answer) + "\n")
-
 
 def readline():
     return sys.stdin.readline()
@@ -39,10 +43,6 @@ if __name__ == "__main__":
                 if next_sequence not in cache:
                     cache[next_sequence] = distance + 1
                     queue.append((next_sequence, distance+1))
-        
-    # with open("temp.json", "w") as f:
-    #     import json
-    #     json.dump(cache, f, sort_keys=True, indent=4)
 
     # read input
     for _ in range(int(readline())):
