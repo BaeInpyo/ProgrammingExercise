e0c4b02751a8aa81ae1caf2a58624952c14609e2
bmy4415 <bmy4415@naver.com>
2020-05-06 21:18:28 +0900

    sjw routing - strange...

--- Chapter30_ShortestPathAlgorithm/Routing/sjw_routing.py
+++ Chapter30_ShortestPathAlgorithm/Routing/sjw_routing.py
@@ -9,22 +9,24 @@ sys.stdin = open(os.path.join(abs_dir, "input.txt"), "r")
 
 # use dijkstra algorithm to find shortest path from 0 to (n-1)
 def solution(n, m, adj_list):
-    dist = [1] + [None] * (n-1)
-    heap = [(1, 0)] # (cost, start)
+    dist = [float("inf")] * n
+    dist[0] = 1.0
+    heap = [(1.0, 0)] # (cost, start)
 
     while heap:
         curr_cost, src = heapq.heappop(heap)
-        if dist[src] is not None and dist[src] < curr_cost:
+        if dist[src] < curr_cost:
             continue
 
-        for (dst, weight) in adj_list[src]:
+        for (dst, weight) in adj_list[src].items():
             next_cost = curr_cost * weight
-            if dist[dst] is None or dist[dst] > next_cost:
+            if next_cost < dist[dst]:
                 dist[dst] = next_cost
                 heapq.heappush(heap, (next_cost, dst))
 
-    answer = str(round(dist[-1], 10)) + "\n"
-    sys.stdout.write(answer)
+    print('%.10f' % (dist[-1]))
+    # answer = str(round(dist[-1], 10)) + "\n"
+    # sys.stdout.write(answer)
 
 
 def readline():
@@ -33,11 +35,11 @@ def readline():
 if __name__ == "__main__":
     for _ in range(int(readline())):
         n, m = [int(x) for x in readline().split()]
-        adj_list = [[] for _ in range(n)]
+        adj_list = [{} for _ in range(n)]
         for _ in range(m):
             a, b, c = readline().split()
             a, b, c = int(a), int(b), float(c)
-            adj_list[a].append((b, c))  # (dst, weight)
-            adj_list[b].append((a, c))  # (dst, weight)
+            adj_list[a][b] = c  # (dst, weight)
+            # adj_list[b][a] = c  # (dst, weight)
 
         solution(n, m, adj_list)
