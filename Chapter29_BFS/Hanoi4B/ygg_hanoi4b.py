5c92784ee3fb2550b009f6658af8d093608ec58d
ene5135 <ene5135@naver.com>
2020-05-10 20:54:44 +0900

    양방향 탐색 구현, 압도적으로빠름, 하지만 hanoi4로 제출했을때 그래도 시간초과

--- Chapter29_BFS/Hanoi4B/ygg_hanoi4b.py
+++ Chapter29_BFS/Hanoi4B/ygg_hanoi4b.py
@@ -24,19 +24,22 @@ def next_state_generator(state):
                 yield ret
 
 def solution(start_state, end_state):
-    bfs_queue = deque([(start_state, 0)])
+    bfs_queue = deque([(start_state, 0, True), (end_state, 0, False)]) # 처음과 끝 양방향에서 출발
     
     while bfs_queue:
-        state, count = bfs_queue.popleft()
+        state, count, direction = bfs_queue.popleft()
         state_key = '|'.join(state)
-        if visit_dict.get(state_key):
-            continue
-        visit_dict[state_key] = True
-        if is_equal(state, end_state):
-            return count
+        if visit_dict.get(state_key) != None: # 현 스태이트를 방문했던 기록이 있으면 
+            visit_count, visit_dir = visit_dict[state_key]
+            if visit_dir == direction: # 현재 방향과 같은 방향에서 방문했던 기록이면 패스
+                continue
+            else: # 반대쪽 방향에서 방문했던 기록이 있으면 여기가 솔루션 포인트
+                return count + visit_count
+            
+        visit_dict[state_key] = (count, direction)
         
         for next_state in next_state_generator(state):
-            bfs_queue.append((next_state, count+1))
+            bfs_queue.append((next_state, count+1, direction))
         
     
 if __name__ == '__main__':
@@ -51,4 +54,5 @@ if __name__ == '__main__':
         n = int(sys.stdin.readline().strip())
         start_state = [' '.join([str_map[i] for i in sys.stdin.readline()[2:].strip().split()]) for _ in range(4)]
         end_state = [' '.join([str_map[i] for i in sys.stdin.readline()[2:].strip().split()]) for _ in range(4)]
+       # end_state = ['','','',' '.join([str_map[str(i)] for i in range(1,n+1)][::-1])]
         print(solution(start_state, end_state))
