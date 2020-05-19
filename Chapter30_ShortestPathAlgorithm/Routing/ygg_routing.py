20bc191ea92160eebd52849f5a57bfc119c0cd48
ene5135 <ene5135@naver.com>
2020-05-20 01:10:44 +0900

    1. math.inf 는 파이썬 3.4.3 에서 안된다. 2. start에서 end로의 직접간선이 최소경로일때를 고려하자

--- Chapter30_ShortestPathAlgorithm/Routing/ygg_routing.py
+++ Chapter30_ShortestPathAlgorithm/Routing/ygg_routing.py
@@ -6,21 +6,25 @@ pop = heapq.heappop
 def solution(N, noise_dict, start, end):
     distance_heap = []
     visited = {start}
+    min_distance_dict = {i:float('inf') for i in range(N)}
     for edge in noise_dict[start]:
+        c, to = edge
         push(distance_heap, (edge))
+        min_distance_dict[to] = c
     
-    while True:
+    while distance_heap:
         min_edge = pop(distance_heap)
         min_c, min_fr = min_edge
         if min_fr == end:
             return min_c
         visited.update([min_fr])
         for c, to in noise_dict[min_fr]:
-            #if to in visited:
-            #    continue
+            if (min_distance_dict[to] <= c*min_c) or (to in visited): # 첫번째 조건 : 이거 안해줘도 민힙이라 상관없는데 민힙 깊이 줄이기위해서 넣어줘 봄
+                continue
             push(distance_heap, (c*min_c, to))
+            min_distance_dict[to] = c*min_c # 이거 안해줘도 민힙이라 상관없는데 민힙 깊이 줄이기위해서 넣어줘 봄
 
-    return -1
+    return min_distance_dict[end] # 이거 해줘야 start 에서 end로의 간선 하나만 있고 경유간선이 하나도 없을때 처리 가능
 if __name__ == '__main__':
     sys.stdin = open('input.in', 'r')
     C = int(sys.stdin.readline().rstrip())
@@ -29,7 +33,8 @@ if __name__ == '__main__':
         noise_dict = {i: [] for i in range(N)}
 
         for _ in range(M):
-            fr, to, c = [float(x) for x in sys.stdin.readline().rstrip().split()]
+            fr, to, c = [x for x in sys.stdin.readline().rstrip().split()]
+            fr, to, c = int(fr), int(to), float(c)
             noise_dict[fr].append((c, to))
             #noise_dict[to].append((c, fr))
 
