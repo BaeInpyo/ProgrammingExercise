7807014d09204d3c6896e2f2fcba5a9eb2ec1106
bmy4415 <bmy4415@naver.com>
2020-06-04 16:30:55 +0900

    sjw - solve reverse string

---
+++ leetcode/june_challenge/sjw_reverse_string.py
@@ -0,0 +1,15 @@
+"""
+Problem URL: https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3350/
+"""
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s)-1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        return
