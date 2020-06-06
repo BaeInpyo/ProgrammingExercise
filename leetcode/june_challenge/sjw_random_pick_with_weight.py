916f2bbf0ffc30afd11358da9eb5dd2fad593ad8
bmy4415 <bmy4415@naver.com>
2020-06-06 12:34:17 +0900

    sjw - solve random pick with weight

---
+++ leetcode/june_challenge/sjw_random_pick_with_weight.py
@@ -0,0 +1,28 @@
+import bisect
+import random
+
+class Solution:
+
+    def __init__(self, w: List[int]):
+        weights = [0]
+        # calculate accumulative sum
+        for weight in w:
+            weights.append(weights[-1] + weight)
+
+        # divide by sum(self.w) which leads to probability
+        weights = [weight / sum(w) for weight in weights]
+        self.weights = weights
+
+    def pickIndex(self) -> int:
+        # pick random value in [0, 1)
+        random_value = random.random()
+
+        # find random value's index
+        index = bisect.bisect_left(weights, random_value)
+
+        return index
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
