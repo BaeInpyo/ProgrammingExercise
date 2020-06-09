9e823fb87edbb9df19f34b6fc85034824dac7dfd
bmy4415 <bmy4415@naver.com>
2020-06-10 00:09:23 +0900

    sjw - solved isSubsequence

---
+++ leetcode/june_challenge/sjw_is_subsequence.py
@@ -0,0 +1,16 @@
+"""
+Problem URL: https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3355/
+"""
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        sidx, tidx = 0, 0
+        while sidx<len(s) and tidx<len(t):
+            print(sidx, tidx)
+            if s[sidx] == t[tidx]:
+                sidx += 1
+                tidx += 1
+            else:
+                tidx += 1
+
+        return sidx == len(s)
