55445e546793bbc851ab4c6bf6ed33a21c06e147
bmy4415 <bmy4415@naver.com>
2020-07-15 16:53:31 +0900

    sjw - solve Same Tree

---
+++ leetcode/july_challenge/same_tree/sjw.py
@@ -0,0 +1,27 @@
+"""
+Problem URL: https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/545/week-2-july-8th-july-14th/3389/
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        # obvious
+        if p is None and q is None:
+            return True
+        if p is None and q is not None:
+            return False
+        if p is not None and q is None:
+            return False
+
+        # Both p and q is not None.
+        if p.val == q.val \
+            and self.isSameTree(p.left, q.left) \
+            and self.isSameTree(p.right, q.right):
+            return True
+
+        return False
