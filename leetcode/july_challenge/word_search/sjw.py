582e8626f30b974e5eaf4e53ad1552663e63de26
bmy4415 <bmy4415@naver.com>
2020-07-21 21:49:53 +0900

    sjw - upgrade Word Search

--- leetcode/july_challenge/word_search/sjw.py
+++ leetcode/july_challenge/word_search/sjw.py
@@ -1,65 +1,47 @@
 """
 Problem URL: https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/546/week-3-july-15th-july-21st/3397/
 
-1. Stort list of positions for each letter.
-2. Start from first character of word, check adjacent letters.
+Do DFS search from all positions
 """
 
 from collections import deque
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        if not word:
-            return True
+        self.m = len(board)
+        self.n = len(board[0])
+        self.board = board
+        self.word = word
+        self.dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # (right, down, left, up)
 
-        # Store starting position of each letter
-        start_position = dict()
-        for idx in range(len(board)):
-            for jdx in range(len(board[idx])):
-                letter = board[idx][jdx]
-                start_position[letter] = start_position.get(letter, [])
-                start_position[letter].append((idx, jdx))
-
-        # Do searching from start position
-        for (idx, jdx) in start_position.get(word[0], []):
-            # Initialize visited
-            visited = []
-            for kdx in range(len(board)):
-                visited.append([False] * len(board[kdx]))
-
-            # print("Start from:", idx, jdx, word)
-            if self.search(board, visited, idx, jdx, 0, word):
-                return True
+        for idx in range(self.m):
+            for jdx in range(self.n):
+                if self.dfs(idx, jdx, 0):
+                    return True
 
         return False
 
-    def search(self, board, visited, idx, jdx, word_index, word):
-        if word[word_index] != board[idx][jdx]:
-            return False
-
-        if word_index == len(word)-1:
-            # print("End from:", idx, jdx)
+    def dfs(self, idx, jdx, wdx):
+        """ Do dfs from (idx, jdx) and return if word[wdx:] is feasible """
+        # Complete word is made
+        if wdx == len(self.word):
             return True
 
-        visited[idx][jdx] = True
-        # print("search from:", idx, jdx)
-        dirs = [
-            (0, 1),
-            (1, 0),
-            (0, -1),
-            (-1, 0),
-        ]
+        # If (idx, jdx) is out of board, return False
+        if not (0 <= idx < self.m) or not (0 <= jdx < self.n):
+            return False
 
-        for (didx, djdx) in dirs:
-            next_idx, next_jdx = idx+didx, jdx+djdx
-            # print("next is:", next_idx, next_jdx)
-            if 0 <= next_idx < len(board) \
-                and 0 <= next_jdx < len(board[idx]) \
-                and not visited[next_idx][next_jdx] \
-                and self.search(
-                    board, visited, next_idx, next_jdx, word_index+1, word):
+        # If current position does not match word, return False
+        if self.board[idx][jdx] != self.word[wdx]:
+            return False
+
+        # This is equivalent with remaining "visited" matrix
+        letter = self.board[idx][jdx]
+        self.board[idx][jdx] = "#"
+        for (dx, dy) in self.dirs:
+            if self.dfs(idx+dx, jdx+dy, wdx+1):
                 return True
 
-        # print("go back from:", idx, jdx)
-        visited[idx][jdx] = False
+        # Recover board
+        self.board[idx][jdx] = letter
         return False
