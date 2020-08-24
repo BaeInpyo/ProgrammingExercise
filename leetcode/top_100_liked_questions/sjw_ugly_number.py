264d276e39817c7541c09bb8d706b6817b607a70
bmy4415 <bmy4415@naver.com>
2020-08-24 21:41:31 +0900

    sjw - solve Ugly Number

--- leetcode/top_100_liked_questions/sjw_ugly_number.py
+++ leetcode/top_100_liked_questions/sjw_ugly_number.py
@@ -4,6 +4,9 @@ Problem URL: https://leetcode.com/problems/ugly-number/
 
 class Solution:
     def isUgly(self, num: int) -> bool:
+        if num <= 0:
+            return False
+            
         factors = [2, 3, 5]
         for factor in factors:
             while num%factor == 0:
