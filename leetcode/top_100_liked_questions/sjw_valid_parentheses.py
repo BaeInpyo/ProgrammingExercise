a11300b6efd0f3c4a698215f95398c9ce2d23ad7
bmy4415 <bmy4415@naver.com>
2020-08-25 16:59:16 +0900

    sjw - solve Valid Parentheses

---
+++ leetcode/top_100_liked_questions/sjw_valid_parentheses.py
@@ -0,0 +1,24 @@
+"""
+Problem URL: https://leetcode.com/problems/valid-parentheses/
+"""
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        match = {
+            ")": "(",
+            "}", "{",
+            "]", "[",
+        }
+        for char in s:
+            if char in ["(", "{", "["]:
+                stack.append(char)
+            elif char in match.keys():
+                if stack and stack[-1] == match[char]:
+                    # parenthesis matches
+                    stack.pop()
+                else:
+                    # stack is empty or parenthesis does not match
+                    return False
+
+        return True
